27db2f50fed6f3a5a8c1988265e1aa308f339e3e
DESKTOP-ET0P0UG\AOT <aot1234>
2020-04-15 18:30:01 +0530

    Read me file and logo images

--- forms-flow-web/src/containers/NavBar.jsx
+++ forms-flow-web/src/containers/NavBar.jsx
@@ -9,7 +9,8 @@ const NavBar = () => {
             <Navbar className="navbar">
                 <section className="container">
                 <Navbar.Brand>
-                    <img className="img-fluid d-none d-md-block" src="/AOT-logo.png" width="300"  alt="AOT Logo" />
+                    <img className="img-fluid d-none d-md-block" src="/AOT-logo.png" width="250"  alt="AOT Logo" />
+                    <img class="img-fluid d-md-none" src="AOT-simple-logo.png" width="40" alt="AOT Logo"></img>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className="navbar-nav">

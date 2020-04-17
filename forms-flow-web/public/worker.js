3881e5127b701ebb88e7802315fbbb831834bdaa
abhilash-aot <abhilash.kr@aot-technologies.com>
2020-04-17 19:57:03 +0530

    Develop pwa (#9)
    * pwa updation
    
    * serice worker updation
    
    Co-authored-by: DESKTOP-ET0P0UG\AOT <aot1234>

--- forms-flow-web/public/worker.js
+++ forms-flow-web/public/worker.js
@@ -1,45 +1,86 @@
-var CACHE_NAME = "REACT_KEYCLOAK";
+var CACHE_NAME = "FORMS-FLOW-WEB";
+// Set this to true for production
+var doCache = true;
+
 var urlsToCache = [
   "/",
+  "/form",
   "/static/js",
   "/static/js/bundle.js",
   "/manifest.json",
   "/favicon.ico",
   "/icons-192.png",
-  "/static/js/0.chunk.js",
   "/static/js/main.chunk.js",
+  "/static/js/0.chunk.js",
   "/keycloak.json",
   "/static/js/0.chunk.js.map",
-  "/bcid-logo-rev-en.svg ",
-  "/bcid-symbol-rev.svg",
-  "/spinner.gif"
+  "/spinner.gif",
+  "/AOT-logo.png",
+  "/AOT-simple-logo.png",
+  "https://iam.aot-technologies.com/auth/"
 ];
-
 // Install a service worker
 self.addEventListener("install", event => {
+  if(doCache)
+  {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       console.log("Opened cache");
-      return cache.addAll(urlsToCache, {
-        mode: "no-cors"
+      cache.addAll(urlsToCache.map(function(urlsToCache) {
+        return new Request(urlsToCache, { mode: 'no-cors' });
+      })).then(function() {
+        console.log('All resources have been fetched and cached.');
       });
     })
   );
+  }
 });
 
-// Cache and return requests
-self.addEventListener("fetch", event => {
+
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
+
+
+// self.addEventListener('fetch', function(event) {
+//   console.log(event.request.url);
+//   event.respondWith(
+//       caches.match(event.request).then(function(response) {
+//           return response || fetch(event.request,{ mode: 'no-cors' });
+//       })
+//   );
+// });
 
 // Update a service worker
 self.addEventListener("activate", event => {

6816bf1b5b8bdf4ccbbe59b95a9d11bc956c9e2d
Rintu Merin <rintu.merin@aot-technologies.com>
2020-04-23 18:52:35 +0530

    User info on the navbar

--- forms-flow-web/src/containers/NavBar.jsx
+++ forms-flow-web/src/containers/NavBar.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { selectRoot } from 'react-formio'
 
 import UserService from '../services/UserService';
-import { STAFF_REVIEWER } from '../constants/constants';
+import { STAFF_REVIEWER, STAFF_DESIGNER, CLIENT } from '../constants/constants';
+
+const getUserRole = (userRoles) => {
+    let role = '';
+    if (userRoles.includes(STAFF_REVIEWER)) {
+        role = "REVIEWER"
+    } else if (userRoles.includes(STAFF_DESIGNER)) {
+        role = "DESIGNER"
+    } else {
+        role = "CLIENT"
+    }
+    return role;
+}
 
 const NavBar = (props) => {
-    const userRoles = props.userRoles;
+    const { user, userRoles } = props;
     return (
         <header>
             <Navbar className="navbar">
@@ -21,13 +33,27 @@ const NavBar = (props) => {
                     <Navbar.Collapse id="responsive-navbar-nav" className="navbar-nav">
                         <Nav className="mr-auto">
                             <Nav.Link as={Link} to='/' className="nav-item nav-link"><i className="fa fa-home" style={{ fontSize: '20px', color: '#ffff' }}></i></Nav.Link>
- {userRoles && userRoles.includes(STAFF_REVIEWER) ?
+                            {userRoles && userRoles.includes(STAFF_REVIEWER) ?
                                 <Nav.Link as={Link} to='/tasks' className="nav-item nav-link"><i className="fa fa-tasks" style={{ fontSize: '15px', color: '#ffff' }}></i></Nav.Link>
                                 :
                                 null}
                         </Nav>
                         <Nav >
-                            <Nav.Link onClick={UserService.userLogout} className="nav-item nav-link" style={{ color: '#ffff' }}><i className="fa fa-power-off" aria-hidden="true"></i> Logout</Nav.Link>
+                            <NavDropdown title={`Hi ${user.given_name ? user.given_name : ''}`} className="nav-dropdown" id="basic-nav-dropdown">
+                                <li className="nav-item">
+                                    <div className="row">
+                                        <p className="col-md-8"></p>
+                                        <p className="col-md-4 userIcon text-center"><i className="fa fa-user-circle" aria-hidden="true"></i></p>
+                                    </div>
+                                    <section className="ml-0">
+                                        <p className="mb-0">{user.name}</p>
+                                        <p className="mb-1" style={{ fontSize: '12px', color: '#807c7c' }}>{user.email}</p>
+                                        <p className="mb-0" style={{color:"#003366cf",fontSize:'14px'}}>{getUserRole(userRoles)}</p>
+                                    </section>
+                                </li>
+                                <NavDropdown.Divider />
+                                <li onClick={UserService.userLogout} style={{cursor:'pointer'}}><i className="fa fa-sign-out" aria-hidden="true"></i>Logout</li>
+                            </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
                 </section>
@@ -38,7 +64,8 @@ const NavBar = (props) => {
 
 const mapStatetoProps = (state) => {
     return {
-        userRoles: selectRoot('user', state).roles || []
+        userRoles: selectRoot('user', state).roles || [],
+        user: selectRoot('user', state).userDetail || []
     }
 }
 

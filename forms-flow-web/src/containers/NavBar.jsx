240c3848da26c5e92386bf831be9a3a4239d9217
Rintu Merin <rintu.merin@aot-technologies.com>
2020-04-28 16:58:22 +0530

    Added CSS for active menu in Navbar

--- forms-flow-web/src/containers/NavBar.jsx
+++ forms-flow-web/src/containers/NavBar.jsx
@@ -1,66 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { selectRoot } from 'react-formio'
 
 import UserService from '../services/UserService';
-import { STAFF_REVIEWER, STAFF_DESIGNER, CLIENT } from '../constants/constants';
+import { STAFF_REVIEWER, STAFF_DESIGNER } from '../constants/constants';
 
-const getUserRole = (userRoles) => {
-    let role = '';
-    if (userRoles.includes(STAFF_REVIEWER)) {
-        role = "REVIEWER"
-    } else if (userRoles.includes(STAFF_DESIGNER)) {
-        role = "DESIGNER"
-    } else {
-        role = "CLIENT"
+class NavBar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isFormActive: true,
+            isTaskActive:false
+        }
     }
-    return role;
-}
+    componentDidMount(){
+        let path = window.location.pathname;
+        let str = path.split('/');
+        this.setActiveMenu(str[1])
+    }
+    setActiveMenu=(path)=>{
+        let data = {...this.state};
+        if(path==="form"){
+            data.isFormActive=true
+            data.isTaskActive=false
+        }else{
+            data.isFormActive=false
+            data.isTaskActive=true
+        }
+        this.setState(data)
+    }
+    getUserRole = (userRoles) => {
+        let role = '';
+        if (userRoles.includes(STAFF_REVIEWER)) {
+            role = "REVIEWER"
+        } else if (userRoles.includes(STAFF_DESIGNER)) {
+            role = "DESIGNER"
+        } else {
+            role = "CLIENT"
+        }
+        return role;
+    }
 
-const NavBar = (props) => {
-    const { user, userRoles } = props;
-    return (
-        <header>
-            <Navbar className="navbar">
-                <section className="container">
-                    <Navbar.Brand>
-                        <img className="img-fluid d-none d-md-block" src="/AOT-logo.png" width="250" alt="AOT Logo" />
-                        <img className="img-fluid d-md-none" src="/AOT-simple-logo.png" width="40" alt="AOT Logo"></img>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" className="navbar-nav">
-                    
-                        <Nav className="mr-auto">
-                            <Nav.Link as={Link} to='/' style={{ fontSize: '20px', color: '#ffff' }} activeclassname="active">Forms</Nav.Link>
-                            {userRoles && userRoles.includes(STAFF_REVIEWER) ?
-                                <Nav.Link as={Link} to='/task'  style={{ fontSize: '20px', color: '#ffff' }} activeclassname="active">Tasks</Nav.Link>
-                                :
-                                null}
-                        </Nav>
-                        <Nav>
-                            <NavDropdown style={{ fontSize: '18px'}} title={<div className="pull-left">
-                            Hi {user.given_name ? user.given_name : ''} &nbsp;
-                        <img className="thumbnail-image" 
-                            src="/assets/Images/user.svg" 
-                            alt="user pic"
-                        /></div>}  className="nav-dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item><img className="float-right" src="/assets/Images/user.svg" alt="userimage"></img></NavDropdown.Item>
-                            <br></br>
-                            <NavDropdown.Header className="nav-user-name">{user.name}</NavDropdown.Header>
-                            <NavDropdown.Header  className="nav-user-email">{user.email}</NavDropdown.Header>
-                            <NavDropdown.Header className="nav-user-role">{getUserRole(userRoles)}</NavDropdown.Header>        
-                                <NavDropdown.Divider />
-                                <NavDropdown.Header className="nav-logout" onClick={UserService.userLogout}><img src="/assets/Images/logout.svg" />Logout</NavDropdown.Header>
+    render() {
+        const { user, userRoles } = this.props;
+        return (
+            <header>
+                <Navbar className="navbar">
+                    <section className="container">
+                        <Navbar.Brand>
+                            <img className="img-fluid d-none d-md-block" src="/AOT-logo.png" width="250" alt="AOT Logo" />
+                            <img className="img-fluid d-md-none" src="/AOT-simple-logo.png" width="40" alt="AOT Logo"></img>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav" className="navbar-nav">
+                            <Nav className="mr-auto">
+                                <Link to="/" className={`nav-link ${this.state.isFormActive? "active":""}`} style={{ fontSize: '20px', color: '#ffff' }} onClick={()=>this.setActiveMenu('form')}>
+                                    Forms
+                                </Link>
+                                {userRoles && userRoles.includes(STAFF_REVIEWER) ?
+                                     <Link to="/task" className={`nav-link ${this.state.isTaskActive? "active":""}`} style={{ fontSize: '20px', color: '#ffff' }} onClick={()=>this.setActiveMenu('task')}>
+                                         Task
+                                    </Link>
+                                    :
+                                    null}
+                            </Nav>
+                            <Nav>
+                                <NavDropdown style={{ fontSize: '18px' }} title={<div className="pull-left">
+                                    Hi {user.given_name ? user.given_name : ''} &nbsp;
+                                    <img className="thumbnail-image"
+                                        src="/assets/Images/user.svg"
+                                        alt="user pic"
+                                    /></div>} className="nav-dropdown" id="basic-nav-dropdown">
+                                    <NavDropdown.Item><img className="float-right" src="/assets/Images/user.svg" alt="userimage"></img></NavDropdown.Item>
+                                    <br></br>
+                                    <NavDropdown.Header className="nav-user-name">{user.name}</NavDropdown.Header>
+                                    <NavDropdown.Header className="nav-user-email">{user.email}</NavDropdown.Header>
+                                    <NavDropdown.Header className="nav-user-role">{this.getUserRole(userRoles)}</NavDropdown.Header>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Header className="nav-logout" onClick={UserService.userLogout}>
+                                        <img src="/assets/Images/logout.svg" alt="" />Logout
+                                </NavDropdown.Header>
 
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </section>
-            </Navbar>
-        </header>
-    )
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </section>
+                </Navbar>
+            </header>
+        )
+    }
 };
 
 const mapStatetoProps = (state) => {

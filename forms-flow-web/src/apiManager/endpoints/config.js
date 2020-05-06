ca5f716ae13d0dba5cafdf4e11dbbad68bac2421
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-05-06 13:19:47 -0700

    Openshift templates/github actions added (#2)
    Openshift templates added
    gh action scripts added

--- forms-flow-web/src/apiManager/endpoints/config.js
+++ forms-flow-web/src/apiManager/endpoints/config.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "/camunda/engine-rest/process-definition/key/"; //process.env.REACT_APP_BPM_API_BASE +
+export const BASE_API_URL = process.env.REACT_APP_BPM_API_BASE+"/camunda/engine-rest/process-definition/key/"; //process.env.REACT_APP_BPM_API_BASE +
 export const BPM_TOKEN_URL = process.env.REACT_APP_BPM_TOKEN_API;
 
 export default {

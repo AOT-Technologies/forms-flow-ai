1a763ee8d7d07fdd952d652892749feb86983f57
abhilash-aot <abhilash.kr@aot-technologies.com>
2020-05-07 13:30:24 +0530

    updated api

--- forms-flow-web/src/apiManager/endpoints/index.js
+++ forms-flow-web/src/apiManager/endpoints/index.js
@@ -7,7 +7,7 @@ const API = {
   GET_TASK_DETAIL_API: BPM_BASE_URL+ '/history/task?taskId=',
   GET_TASK_COUNT: BPM_BASE_URL+'/history/task/count',
   TASK_ACTION_API: BPM_BASE_URL+`/task`,
-  GET_TASK_SUBMISSION_DATA: BPM_BASE_URL+'/history/variable-instance?process-instance='
+  GET_TASK_SUBMISSION_DATA: BPM_BASE_URL+'/history/variable-instance?processInstanceId='
 }
 //TODO update the process to read from json/API
 

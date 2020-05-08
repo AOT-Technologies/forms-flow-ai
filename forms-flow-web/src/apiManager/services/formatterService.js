59bd6ec5e82355fc689ed5aa4302b39a14f3a913
Rintu Merin <rintu.merin@aot-technologies.com>
2020-05-08 17:50:06 +0530

    Removed task files

--- forms-flow-web/src/apiManager/services/formatterService.js
+++
@@ -1,5 +0,0 @@
-export const taskSubmissionFormatter = (taskSubmissionData) =>{
-  const res = {};
-  taskSubmissionData.forEach(taskSubmission => res[taskSubmission.name] = taskSubmission.value);
-  return res;
-}

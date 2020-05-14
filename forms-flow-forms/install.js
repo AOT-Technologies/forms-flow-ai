2f85deff714d72dc186638013f8d3ca3f1014259
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-05-13 21:05:45 -0700

    adding domain name

--- forms-flow-forms/install.js
+++ forms-flow-forms/install.js
@@ -145,7 +145,7 @@ module.exports = function(formio, items, done) {
       // Change the project configuration.
       const config = fs.readFileSync(path.join(directoryPath, 'config.template.js'));
       const newConfig = nunjucks.renderString(config.toString(), {
-        domain: formio.config.domain ? formio.config.domain : 'https://form.io'
+        domain: process.env.DOMAIN ? process.env.DOMAIN: formio.config.domain
       });
       fs.writeFileSync(path.join(directoryPath, 'config.js'), newConfig);
       done();

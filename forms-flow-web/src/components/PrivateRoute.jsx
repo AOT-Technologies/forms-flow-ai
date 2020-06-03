5660828e26013cf356e3246a1b035af57863372f
Rintu Merin <rintu.merin@aot-technologies.com>
2020-06-03 18:16:35 +0530

    Task route set for reviewer alone

--- forms-flow-web/src/components/PrivateRoute.jsx
+++ forms-flow-web/src/components/PrivateRoute.jsx
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import UserService from '../services/UserService'
-import { Route, Redirect } from 'react-router-dom';
 import Form from './Form';
 import Task from './Task';
 import { setUserAuth } from '../actions/bpmActions';
-import { connect } from 'react-redux';
+import { STAFF_REVIEWER } from '../constants/constants';
 
-class PrivateRoute extends Component{
-    componentDidMount(){
-        UserService.initKeycloak(this.props.store,(err,res)=>{
-          this.props.setUserAuth(res.authenticated)
-        })
-      }
+class PrivateRoute extends Component {
+  componentDidMount() {
+    UserService.initKeycloak(this.props.store, (err, res) => {
+      this.props.setUserAuth(res.authenticated)
+    })
+  }
+  TaskRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      this.props.user.includes(STAFF_REVIEWER)
+        ? <Component {...props} />
+        : <Redirect exact to='/' />
+    )} />
+  )
 
-    render(){
-        return(
-            <div className="container">
-                <Route path="/form" component={Form}/>
-                <Route path="/task" component={Task}/>
-                <Route exact path="/"><Redirect to="/form"/></Route>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <Route path="/form" component={Form} />
+        <this.TaskRoute path="/task" component={Task} />
+        <Route exact path="/"><Redirect to="/form" /></Route>
+      </div>
+    )
+  }
 }
 
-const mapStateToDispatch = (dispatch) =>{
-    return{
-      setUserAuth:(value)=>{
-        dispatch(setUserAuth(value))
-      }
+const mapStatetoProps = (state) => {
+  return {
+    user: state.user.roles || [],
+  }
+}
+
+const mapStateToDispatch = (dispatch) => {
+  return {
+    setUserAuth: (value) => {
+      dispatch(setUserAuth(value))
     }
   }
+}
 
-export default connect(null,mapStateToDispatch)(PrivateRoute);
+export default connect(mapStatetoProps, mapStateToDispatch)(PrivateRoute);

4aa5ff786b3aba879e79fc40e6c434cd57aa57e4
guru-aot <62901416+guru-aot@users.noreply.github.com>
2020-06-30 12:43:02 +0530

    Update Insights.js

--- forms-flow-web/src/components/Insights/Insights.js
+++ forms-flow-web/src/components/Insights/Insights.js
@@ -9,12 +9,12 @@ const Insights = (props) => {
   //TODO Remove once API cors fixes
   const options = [
     {
-      value: 'dashboard1',
+      value: 'public-notification',
       label: 'RPAS Self Assessment Dashboard',
       "public_url": "https://analytics1.aot-technologies.com/public/dashboards/3REIAWpfBZ9kW1y924BFknB1QGIQLozRedi9E11U?org_slug=default"
     },
     {
-      value: 'foi-submissions',
+      value: 'foi-notification',
       label: 'FOI Submissions',
       "public_url": "https://analytics1.aot-technologies.com/public/dashboards/H7xv90X2CmpXxJecSCFv1Ca6DrHHptnqaCrpQwv7?org_slug=default"
     }

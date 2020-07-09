bb5ffc1a7dc551cea845a7026c33763515224f73
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2020-07-09 15:19:58 -0700

    Changes to use user token.

--- forms-flow-webapi/src/forms_flow_api/services/external/base_bpm.py
+++ forms-flow-webapi/src/forms_flow_api/services/external/base_bpm.py
@@ -5,16 +5,16 @@ import json
 import requests
 from flask import current_app
 
-from ...utils.logging import log_error
+from ...utils.logging import log_error, log_info
 
 
 class BaseBPMService():
     """This class manages all of the base externel service calls."""
 
     @classmethod
-    def get_request(cls, url):
+    def get_request(cls, url, token):
         """Get HTTP request to BPM API with auth header."""
-        headers = cls._get_headers_()
+        headers = cls._get_headers_(token)
         response = requests.get(url, headers=headers)
 
         data = None
@@ -26,9 +26,9 @@ class BaseBPMService():
         return data
 
     @classmethod
-    def post_request(cls, url, payload=None):
+    def post_request(cls, url, token, payload=None):
         """Post HTTP request to BPM API with auth header."""
-        headers = cls._get_headers_()
+        headers = cls._get_headers_(token)
         payload = json.dumps(payload) if payload else payload
         response = requests.post(url, data=payload, headers=headers)
 
@@ -44,7 +44,7 @@ class BaseBPMService():
         return data
 
     @classmethod
-    def _get_headers_(cls):
+    def _get_headers_(cls, token):
         """Generate headers."""
         bpm_token_api = current_app.config.get('BPM_TOKEN_API')
         bpm_client_id = current_app.config.get('BPM_CLIENT_ID')
@@ -57,10 +57,17 @@ class BaseBPMService():
             'client_secret': bpm_client_secret,
             'grant_type': bpm_grant_type
         }
-        response = requests.post(bpm_token_api, headers=headers, data=payload)
-        data = json.loads(response.text)
-
-        return {
-            'Authorization': 'Bearer ' + data['access_token'],
-            'Content-Type': 'application/json'
-        }
+        if token:
+            log_info('reuse token')
+            return {
+                'Authorization': token,
+                'content-type': 'application/json'
+            }
+        else:
+            log_info('new token')
+            response = requests.post(bpm_token_api, headers=headers, data=payload)
+            data = json.loads(response.text)
+            return {
+                'Authorization': 'Bearer ' + data['access_token'],
+                'Content-Type': 'application/json'
+            }
--- forms-flow-webapi/src/forms_flow_api/services/external/bpm.py
+++ forms-flow-webapi/src/forms_flow_api/services/external/bpm.py
@@ -21,78 +21,78 @@ class BPMService(BaseBPMService):
     """This class manages all of the Camunda BPM Service."""
 
     @classmethod
-    def get_all_process(cls):
+    def get_all_process(cls, token):
         """Get all process."""
         url = cls._get_url_(BPMEndpointType.ProcessDefinition)
-        return cls.get_request(url)
+        return cls.get_request(url, token)
 
     @classmethod
-    def get_process_details(cls, process_key):
+    def get_process_details(cls, process_key, token):
         """Get process details."""
         url = cls._get_url_(BPMEndpointType.ProcessDefinition) + process_key
-        return cls.get_request(url)
+        return cls.get_request(url, token)
 
     @classmethod
-    def get_process_actions(cls, process_key):
+    def get_process_actions(cls, process_key, token):
         """Get process actions."""
         url = cls._get_url_(BPMEndpointType.ProcessDefinition) + process_key
-        return cls.get_request(url)
+        return cls.get_request(url, token)
 
     @classmethod
-    def post_process_evaluate(cls, payload):
+    def post_process_evaluate(cls, payload, token):
         """Get states by evaluating process."""
         url = f'{cls._get_url_(BPMEndpointType.DecisionDefinition)}key/state-decision/evaluate'
-        return cls.post_request(url, payload=payload)
+        return cls.post_request(url, token, payload=payload)
 
     @classmethod
-    def post_process_start(cls, process_key, payload):
+    def post_process_start(cls, process_key, payload, token):
         """Post process start."""
         url = f'{cls._get_url_(BPMEndpointType.ProcessDefinition)}key/{process_key}/start'
-        return cls.post_request(url, payload=payload)
+        return cls.post_request(url, token, payload=payload)
 
     @classmethod
-    def get_all_tasks(cls):
+    def get_all_tasks(cls, token):
         """Get all tasks."""
         url = cls._get_url_(BPMEndpointType.History) + 'task'
-        return cls.get_request(url)
+        return cls.get_request(url, token)
 
     @classmethod
-    def get_task(cls, task_id):
+    def get_task(cls, task_id, token):
         """Get task."""
         url = cls._get_url_(BPMEndpointType.History) + 'task?taskId=' + task_id
-        return cls.get_request(url)
+        return cls.get_request(url, token)
 
     @classmethod
-    def get_task_variables(cls, process_instance_id):
+    def get_task_variables(cls, process_instance_id, token):
         """Get task details."""
         url = cls._get_url_(BPMEndpointType.History) + 'variable-instance'
         data = {}
         data['processInstanceId'] = process_instance_id
-        return cls.post_request(url, data)
+        return cls.post_request(url, token, data)
 
     @classmethod
-    def claim_task(cls, task_id, data):
+    def claim_task(cls, task_id, data, token):
         """Claim a task."""
         url = cls._get_url_(BPMEndpointType.Task) + task_id + '/claim'
-        return cls.post_request(url, data)
+        return cls.post_request(url, token, data)
 
     @classmethod
-    def unclaim_task(cls, task_id, data):
+    def unclaim_task(cls, task_id, data, token):
         """Unclaim a task."""
         url = cls._get_url_(BPMEndpointType.Task) + task_id + '/unclaim'
-        return cls.post_request(url, data)
+        return cls.post_request(url, token, data)
 
     @classmethod
-    def complete_task(cls, task_id, data):
+    def complete_task(cls, task_id, data, token):
         """Complete a task."""
         url = cls._get_url_(BPMEndpointType.Task) + task_id + '/complete'
-        return cls.post_request(url, data)
+        return cls.post_request(url, token, data)
 
     @classmethod
-    def trigger_notification(cls):
+    def trigger_notification(cls, token):
         """Submit a form."""
         url = cls._get_url_(BPMEndpointType.ProcessDefinition) + 'process/start'
-        return cls.post_request(url)
+        return cls.post_request(url, token)
 
     @classmethod
     def _get_url_(cls, endpoint_type: BPMEndpointType):

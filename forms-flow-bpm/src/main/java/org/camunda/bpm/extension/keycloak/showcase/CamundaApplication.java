8f92c6dd8b1a1a0ed5bc74e0edb8ed6577f03814
chunthebear <zyc5026@hotmail.com>
2020-07-13 14:38:50 -0700

    Added automation for authorizing service account
    The current authorization is to give the service account all permissions to all resources except for Process Instance and Process Definition. For these two, a GLOBAL permission is given to ALL users (marked as *).

--- forms-flow-bpm/src/main/java/org/camunda/bpm/extension/keycloak/showcase/CamundaApplication.java
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/keycloak/showcase/CamundaApplication.java
@@ -1,5 +1,9 @@
 package org.camunda.bpm.extension.keycloak.showcase;
 
+import org.camunda.bpm.engine.AuthorizationService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.authorization.Authorization;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
 import org.slf4j.Logger;
@@ -19,6 +23,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 import javax.sql.DataSource;
 import java.util.Properties;
+
+import static org.camunda.bpm.engine.authorization.Permissions.ALL;
+import static org.camunda.bpm.engine.authorization.Authorization.ANY;
+import static org.camunda.bpm.engine.authorization.Authorization.AUTH_TYPE_GRANT;
+import static org.camunda.bpm.engine.authorization.Authorization.AUTH_TYPE_GLOBAL;
 
 /**
  * The Camunda Showcase Spring Boot application.
@@ -41,7 +50,9 @@ public class CamundaApplication {
 	public void onPostDeploy(PostDeployEvent event) {
 		LOG.info("========================================");
 		LOG.info("Successfully started Camunda Showcase");
-		LOG.info("========================================");
+		LOG.info("Customized by AOT Technologies");
+		LOG.info("========================================\n");
+		authorizeServiceAccount();
 	}
 	
 	/**
@@ -71,4 +82,29 @@ public class CamundaApplication {
 		return new Properties();
 	}
 
+	private static void authorizeServiceAccount() {
+		// Add authorization to service account
+		LOG.info("Setting authorization for service account...");
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+		/* To read about resource type in Camunda, refer to
+		   https://docs.camunda.org/manual/latest/user-guide/process-engine/authorization-service/#resources */
+		for (int resourceType = 0; resourceType<=17; resourceType++){
+			LOG.info("==> Setting resource type " + resourceType);
+			AuthorizationService authorizationService = processEngine.getAuthorizationService();
+			Authorization auth;
+			// If resource is process defination or instance, set GLOBAL; Else, ALLOW
+			if (resourceType == 6 || resourceType == 8) {
+				auth = authorizationService.createNewAuthorization(AUTH_TYPE_GLOBAL);
+				auth.setUserId("*");
+			} else {
+				auth = authorizationService.createNewAuthorization(AUTH_TYPE_GRANT);
+				auth.setUserId("service-account-"+System.getenv("KEYCLOAK_CLIENTID"));
+			}
+			auth.setResourceType(resourceType);
+			auth.setResourceId(ANY);
+			auth.addPermission(ALL);
+			authorizationService.saveAuthorization(auth);
+		}
+		LOG.info("Authorization set!\n");
+	}
 }

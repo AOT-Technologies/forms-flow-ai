b60910a25c93f45f24490da5956985bc5a531606
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2020-07-21 12:22:18 -0700

    Structural changes.

--- forms-flow-api/wsgi.py
+++ forms-flow-api/wsgi.py
@@ -1,6 +1,6 @@
 """Provides the WSGI entry point for running the application."""
 
-from forms_flow_api import create_app
+from api import create_app
 
 
 application = create_app()  # pylint: disable=invalid-name

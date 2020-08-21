b8210f413506f40cd088b2a98b8e4c0d807ad0e3
chunthebear <zyc5026@hotmail.com>
2020-08-21 13:40:40 -0700

    Improved and rearranged OpenShift templates; Removed CORS changes

--- forms-flow-api/src/api/__init__.py
+++ forms-flow-api/src/api/__init__.py
@@ -3,7 +3,6 @@
 import os
 
 from flask import Flask
-# from flask_cors import CORS
 
 from . import config, models
 from .models import db, ma
@@ -19,8 +18,6 @@ setup_logging(os.path.join(os.path.abspath(
 def create_app(run_mode=os.getenv('FLASK_ENV', 'production')):
     """Return a configured Flask App using the Factory method."""
     app = Flask(__name__)
-    # CORS(app)
-    # app.config['CORS_HEADERS'] = 'Content-Type'
     app.config.from_object(config.CONFIGURATION[run_mode])
 
     db.init_app(app)

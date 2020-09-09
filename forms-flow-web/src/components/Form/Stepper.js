5fda6b9483278bd4dbf4bd2baebaef776a724964
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2020-09-09 09:57:07 -0700

    stepper

--- forms-flow-web/src/components/Form/Stepper.js
+++ forms-flow-web/src/components/Form/Stepper.js
@@ -32,7 +32,7 @@ class StepperPage extends Component {
     super(props);
     this.state = {
       checked: false,
-      activeStep: 2,
+      activeStep: 0,
       workflow: null,
       status: null,
       previewMode: false,

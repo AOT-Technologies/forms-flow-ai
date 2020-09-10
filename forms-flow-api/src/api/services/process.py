7b35f8034269b6909314d1a185fa87f1911c62f7
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2020-09-10 12:45:14 -0700

    Resolution of merge conflict.

--- forms-flow-api/src/api/services/process.py
+++ forms-flow-api/src/api/services/process.py
@@ -16,10 +16,19 @@ class ProcessService():
     def get_all_processes(token):
         """Get all processes."""
         process = BPMService.get_all_process(token)
-        logging.log(logging.INFO, process)
         if process:
-            return ProcessListSchema().dump(process,many=True)
-        raise BusinessException('Invalid Request', HTTPStatus.BAD_REQUEST)
+            result = ProcessListSchema().dump(process)
+            seen = set()
+            new_result = []
+            for data in result:
+                for d in data:
+                    t = tuple(d.items())
+                    if t not in seen:
+                        seen.add(t)
+                        new_result.append(d)
+            return new_result
+
+        return process
 
     @staticmethod   
     def get_process(process_key, token):

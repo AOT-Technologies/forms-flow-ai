0dd0e6b9b2190ab66cc147a5cb5fb4d20954e1cc
abhilash-aot <abhilash.kr@aot-technologies.com>
2020-09-10 12:26:57 +0530

    window env check

--- forms-flow-web/src/config.js
+++ forms-flow-web/src/config.js
@@ -1,5 +1,5 @@
-let PROJECT_URL = window._env_.REACT_APP_API_PROJECT_URL || process.env.REACT_APP_API_PROJECT_URL || 'http://127.0.0.1:3001';
-let API_URL = window._env_.REACT_APP_API_SERVER_URL || process.env.REACT_APP_API_SERVER_URL || 'http://127.0.0.1:3001';
+let PROJECT_URL = window._env_?.REACT_APP_API_PROJECT_URL || process.env.REACT_APP_API_PROJECT_URL || 'http://127.0.0.1:3001';
+let API_URL = window._env_?.REACT_APP_API_SERVER_URL || process.env.REACT_APP_API_SERVER_URL || 'http://127.0.0.1:3001';
 
 let query = {};
 window.location.search.substr(1).split('&').forEach(function(item) {

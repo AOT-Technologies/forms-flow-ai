78e541eedce37b55502cc58937f74bd5435879a4
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2020-09-10 17:09:47 -0700

    stepper edit

--- forms-flow-web/src/apiManager/endpoints/config.js
+++ forms-flow-web/src/apiManager/endpoints/config.js
@@ -1,14 +1,11 @@
-const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
-export const INSIGHTS_BASE_API = `${PROXY_URL}${
-  (window._env_ && window._env_.REACT_APP_INSIGHT_API_BASE) ||
-  process.env.REACT_APP_INSIGHT_API_BASE
-}/api`;
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+export const INSIGHTS_BASE_API =
+  `${PROXY_URL}${window._env_.REACT_APP_INSIGHT_API_BASE ||
+  process.env.REACT_APP_INSIGHT_API_BASE}/api`;
 export const INSIGHTS_API_KEY =
-  (window._env_ && window._env_.REACT_APP_INSIGHTS_API_KEY) ||
+  window._env_.REACT_APP_INSIGHTS_API_KEY ||
   process.env.REACT_APP_INSIGHTS_API_KEY;
-export const WEB_BASE_URL =
-  (window._env_ && window._env_.REACT_APP_WEB_BASE_URL) ||
-  process.env.REACT_APP_WEB_BASE_URL;
+export const WEB_BASE_URL = window._env_.REACT_APP_WEB_BASE_URL || process.env.REACT_APP_WEB_BASE_URL;
 
 export default {
   INSIGHTS_BASE_API,

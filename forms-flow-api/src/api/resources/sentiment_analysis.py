e3a63b5f214c08fd4216f009f9a0957814351acf
kurianbenoy <kurian.benoy@aot-technologies.com>
2020-09-18 00:27:30 +0530

    update sentiment_analysis schema code

--- forms-flow-api/src/api/resources/sentiment_analysis.py
+++ forms-flow-api/src/api/resources/sentiment_analysis.py
@@ -7,9 +7,10 @@ from ..schemas import SentimentAnalysisSchema
 from ..services.sentiment_analysis import sentiment_pipeline
 
 
-API = Namespace('sentiment', description='API endpoint for sentiment analysis')
+API = Namespace("sentiment", description="API endpoint for sentiment analysis")
 
-@API.route('/insert', methods=['POST', 'GET'])
+
+@API.route("/insert", methods=["POST", "GET"])
 def sentiment_analysis_mongodb_insert():
     """ Api for adding sentiment analysis response to mongodb """
     parsejson = request.get_json()
@@ -18,10 +19,10 @@ def sentiment_analysis_mongodb_insert():
     output_response = jsonify(response)
     post_data = {input: text, output: output_response}
     schema = Database.create(post_data)
-    return 'Data was entered into mongo db database'
+    return "Data was entered into mongo db database"
 
 
-@API.route('/api', methods=['POST', 'GET'])
+@API.route("/api", methods=["POST", "GET"])
 def sentiment_analysis_api():
     """ Api for fetching sentiment analysis response"""
     parsejson = request.get_json()
--- forms-flow-api/src/api/schemas/sentiment_analysis.py
+++ forms-flow-api/src/api/schemas/sentiment_analysis.py
@@ -5,19 +5,16 @@ from ..models import mongo
 
 class SentimentAnalysisSchema(object):
     """Schema for creating pymongo database instance
-    with fields  
+    with fields
     methods like update, find, delete can be added later
     """
+
     def __init__(self):
-        self.fields = {
-            "input_text": "string" ,
-            "output_response": "collection" 
-        }
-       
+        self.fields = {"input_text": "string", "output_response": "collection"}
 
     def insert(self, sentiment_object):
         # TODO: validate if all the object of database are currently there in required fields
         mongo_db = mongo.db.application_ai
         inserted = mongo_db.insert(sentiment_object)
-        id =  str(inserted.inserted_id)
+        id = str(inserted.inserted_id)
         return f"Mongodb response created for {id}"

295abc7a7fb925cf0a6b8442c26c02b5220cd485
sumathi-thirumani-aot <57740789+sumathi-thirumani-aot@users.noreply.github.com>
2020-09-28 00:29:52 -0700

    Update sentiment_analysis.py

--- forms-flow-api/src/api/services/sentiment_analysis.py
+++ forms-flow-api/src/api/services/sentiment_analysis.py
@@ -1,7 +1,7 @@
 import spacy
 import nltk
 
-import os
+from pathlib import Path
 
 nltk.downloader.download("vader_lexicon")
 nltk.downloader.download("punkt")
@@ -54,7 +54,7 @@ class SentimentAnalyserService():
 def load_model_output(text):
     # uncomment when working in linux and remove subsequent two lines
     # nlp = spacy.load("../models/quick-spacy/")
-    model_path = Path(os.path.dirname(os.path.realpath(__file__))+"/models/quick-spacy/")
+    model_path = Path(__file__).parent.absolute()/'models/quick-spacy/'
     nlp = spacy.load(model_path)
     doc = nlp(text)
     sentence = [ent.text for ent in doc.ents]

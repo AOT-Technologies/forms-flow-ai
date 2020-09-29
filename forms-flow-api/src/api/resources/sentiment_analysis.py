1c2575c0ee89e2d020a555eaf9cbee957cef8d34
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2020-09-29 16:08:21 -0700

    Support for multiple records + Resolution of 500 for invalid input.

--- forms-flow-api/src/api/resources/sentiment_analysis.py
+++ forms-flow-api/src/api/resources/sentiment_analysis.py
@@ -13,7 +13,6 @@ from ..utils.util import cors_preflight
 import json
 
 
-
 API = Namespace("sentiment", description="API endpoint for sentiment analysis")
 
 
@@ -25,39 +24,26 @@ class SentimentAnalysisResource(Resource):
     @cors.crossdomain(origin='*')
     @auth.require
     def post():
-        inputjson = request.get_json()
-        for inputs in inputjson["data"]:
-            text = inputs["text"]
-            topics = inputs["topics"]
-            inputs["applicationId"] = inputjson["applicationId"]
-            inputs["formUrl"] = inputjson["formUrl"]
+        inputJson = request.get_json()
+        responseJson = {"applicationId": inputJson["applicationId"], "formUrl" : inputJson["formUrl"], "data":[]}
+        for data in inputJson["data"]:
+            text = data["text"]
+            topics = data["topics"]
+            dataInput = {"applicationId": inputJson["applicationId"], "formUrl" : inputJson["formUrl"], "elementId" : data["elementId"], "topics": topics, "text":text}
+
             # processing topics in ML model format
             new_topics = [t[:3].upper() for t in topics]
 
             response = SentimentAnalyserService.sentiment_pipeline(text=text)
+            response["elementId"] = data["elementId"]
             if response["sentiment"]=={}:
-                response_data = {"sentiment": "null", "overall_sentiment":response["overall_sentiment"]}
-                response_data["applicationId"] = inputjson["applicationId"]
-                response_data["formUrl"] = inputjson["formUrl"]
-                response_data["elementId"] = inputs["elementId"]
-                post_data = {"input_text": inputs, "output_response": response_data}
-                db_instance = SentimentAnalysisSchema()
-                result = db_instance.insert_sentiment(post_data)
+                response["sentiment"] = None
 
-            else:
-                response["applicationId"] = inputjson["applicationId"]
-                response["formUrl"] = inputjson["formUrl"]
-                response["elementId"] = inputs["elementId"]
-                output_response = jsonify(response)
+            responseJson["data"].append(dict(response))
+            response["applicationId"] = inputJson["applicationId"]
+            response["formUrl"] = inputJson["formUrl"]
+            post_data = {"input_text": dataInput, "output_response": response}
+            db_instance = SentimentAnalysisSchema()
+            db_instance.insert_sentiment(post_data)
 
-                post_data = {"input_text": inputs, "output_response": response}
-                db_instance = SentimentAnalysisSchema()
-                result = db_instance.insert_sentiment(post_data)
-
-                entity_response = entity_category(text, new_topics)
-                if entity_response==[]:
-                    pass
-                else:
-                    db_entity_instance = SentimentAnalysisSchema()
-                    db_entity_instance.insert_entity(entity_response)
-                    return output_response, HTTPStatus.OK
+        return jsonify(responseJson), HTTPStatus.OK

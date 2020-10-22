b4d6f666006badfa35856be153d59a3d24f407e9
twinkleaot <twinkle.joseph@gmail.com>
2020-10-22 11:25:00 -0700

    Issue fixing: size of content of metrics page

--- forms-flow-web/src/components/Dashboard/Dashboard.js
+++ forms-flow-web/src/components/Dashboard/Dashboard.js
@@ -85,9 +85,9 @@ const Dashboard = () => {
       <div className="dashboard mb-2">
         <div className="row ">
           <div className="col-12">
-            <h1 className="dashboard-title">
+            <h3 className="dashboard-title">
               <i className="fa fa-pie-chart" aria-hidden="true"/> Metrics
-            </h1>
+            </h3>
             <hr className="line-hr"/>
             <div className="row ">
               <div className="col-12 col-lg-6 ">

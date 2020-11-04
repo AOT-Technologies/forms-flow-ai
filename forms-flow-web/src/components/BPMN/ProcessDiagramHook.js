ade73a69c44cd9b9ad04cbe4e36e71d9d5721ec7
abhilash-aot <abhilash.kr@aot-technologies.com>
2020-11-04 09:48:25 +0530

    reduntant calls avoided

--- forms-flow-web/src/components/BPMN/ProcessDiagramHook.js
+++ forms-flow-web/src/components/BPMN/ProcessDiagramHook.js
@@ -10,8 +10,9 @@ import {setProcessDiagramLoading} from "../../actions/processActions";
 import "./bpm.scss"
 import BpmnJS from 'bpmn-js';
 
-function ProcessDiagram(props){
+const ProcessDiagram = (props)=>{
   const process_key = props.process_key;
+  console.log("start",process_key);
   const containerRef = useRef(null);
   console.log('containerRef current>>>>>>',containerRef.current);
   console.log('containerRef >>>>>>',containerRef);
@@ -22,51 +23,48 @@ function ProcessDiagram(props){
   const container = containerRef.current;
   const bpmnViewer = new BpmnJS({ container });
   console.log('container >>>>>>',container);
- 
-  
 
-  useEffect(()=>{  
-    const displayDiagram = (diagramXML)=>{
-      console.log('container 3>>>>>>',container);
-      // console.log('diagramXML >>>>>>',diagramXML);
-      
-      bpmnViewer.on('import.done', (event) => {
-        console.log('import.done >>>>>>',event);
-        const {
-          error,
-          warnings
-        } = event;
-        if (error) {
-          console.log('inside bpmnViewer on error >', error);
-          //return handleError(error);
-        }
-        bpmnViewer.get('canvas').zoom('fit-viewport');
-        //return handleShown(warnings);
-      });
-      bpmnViewer.importXML(diagramXML);
-    }
 
+
+  useEffect(()=>{
     dispatch(setProcessDiagramLoading(true));
     // console.log('process_key >>',process_key);
-    
     if(process_key){
       dispatch(fetchDiagram(process_key));
       //console.log('diagramXML >>',diagramXML);
-      
     }
-    console.log('diagramXML',diagramXML);
-    if(diagramXML){
-      
-      displayDiagram(diagramXML);
-    }
-    console.log('containerRef current 2>>>>>>',containerRef.current);
-  },[process_key,diagramXML,dispatch,bpmnViewer,container])
+  },[process_key,dispatch])
 
- 
+ useEffect(()=>{
+   console.log('diagramXML >>',diagramXML)
+   const displayDiagram = (diagramXML)=>{
+     console.log('container 3>>>>>>',container);
+     // console.log('diagramXML >>>>>>',diagramXML);
+
+     bpmnViewer.on('import.done', (event) => {
+       console.log('import.done >>>>>>',event);
+       const {
+         error,
+         warnings
+       } = event;
+       if (error) {
+         console.log('inside bpmnViewer on error >', error);
+         //return handleError(error);
+       }
+       bpmnViewer.get('canvas').zoom('fit-viewport');
+       //return handleShown(warnings);
+     });
+     bpmnViewer.importXML(diagramXML);
+   }
+   if(diagramXML){
+     displayDiagram(diagramXML);
+   }
+   console.log('containerRef current 2>>>>>>',containerRef.current);
+ },[diagramXML,bpmnViewer, container]);
 
 
-  
-  const handleError = (err) => {
+
+  /*const handleError = (err) => {
     console.log(err);
     const { onError } = props;
     if (onError) {
@@ -79,7 +77,7 @@ function ProcessDiagram(props){
     if (onShown) {
       onShown(warnings);
     }
-  }
+  }*/
 
   if (isProcessDiagramLoading) {
     return <Loading/>;

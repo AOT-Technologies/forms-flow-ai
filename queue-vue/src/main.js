03a7e3e0cd2659450959f548c217c7f4a4ea6fac
kurianbenoy <kurian.benoy@aot-technologies.com>
2021-02-09 10:51:20 +0530

    add vuex store boiler code and remove token generation of formio

--- queue-vue/src/main.js
+++ queue-vue/src/main.js
@@ -1,40 +1,59 @@
 import VeeValidate from 'vee-validate';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Vue from 'vue';
 import VueLogger from 'vuejs-logger';
 import App from './App';
-// import authenticateFormio from './services/formio';
 import router from './router';
+import store from './store';
+import BootstrapVue from 'bootstrap-vue';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Keycloak from 'keycloak-js';
 
-Vue.config.productionTip = false;
-const options = {
-  isEnabled: true,
-  logLevel : Vue.config.productionTip  ? 'error' : 'debug',
-  stringifyArguments : false,
-  showLogLevel : true,
-  showMethodName : true,
-  separator: '|',
-  showConsoleColors: true
-};
-Vue.use(VueLogger, options);
-Vue.use(VeeValidate);
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-/* eslint-disable no-new */
 
+const options = {
+  isEnabled: true, logLevel : Vue.config.productionTip  ? 'error' : 'debug', stringifyArguments : false, showLogLevel : true, showMethodName : true, separator: '|', showConsoleColors: true
+};
+
+Vue.config.productionTip = false;
+Vue.use(VueLogger, options);
+Vue.use(BootstrapVue)
+Vue.use(VeeValidate)
+
+// var jwt = rsequire("jsonwebtoken");
 let initOptions = {
   url: process.env.VUE_APP_KEYCLOAK_URL,
   realm: process.env.VUE_APP_KEYCLOAK_REALM ,
   clientId: process.env.VUE_APP_KEYCLOAK_CLIENT_ID ,
   onLoad: 'login-required'
+  // onLoad: 'check-sso'
 }
 
+// const STAFF_REVIEWER_ID = process.env.VUE_APP_REVIEWER_ROLE_ID
+// const STAFF_REVIEWER = process.env.VUE_APP_REVIEWER_ROLE
 
-let keycloak = Keycloak(initOptions)
+// const STAFF_DESIGNER_ID = process.env.VUE_APP_DESIGNER_ROLE_ID
+// const STAFF_DESIGNER = process.env.VUE_APP_DESIGNER_ROLE
+
+// const CLIENT_ID = process.env.VUE_APP_CLIENT_ROLE_ID
+// const CLIENT = process.env.VUE_APP_CLIENT_ROLE
+
+// const ROLES = [
+//   {
+//     id: STAFF_REVIEWER_ID,
+//     title: STAFF_REVIEWER,
+//   },
+//   {
+//     id: STAFF_DESIGNER_ID,
+//     title: STAFF_DESIGNER,
+//   },
+//   {
+//     id: CLIENT_ID,
+//     title: CLIENT,
+//   },
+// ];
+
+let keycloak = Keycloak(initOptions);
 
 keycloak.init({ onLoad: initOptions.onLoad }).then((auth) =>{
     
@@ -44,33 +63,42 @@ keycloak.init({ onLoad: initOptions.onLoad }).then((auth) =>{
     Vue.$log.info("Authenticated");
   }
 
-  let ANONYMOUS_ID = process.env.VUE_APP_ANONYMOUS_ID
-  const roles = [ANONYMOUS_ID];
-
-  var jwt = require('jsonwebtoken');
-  const USER_RESOURCE_FORM_ID = process.env.VUE_APP_USER_RESOURCE_FORM_ID
-
-  var token = jwt.sign(
-    {
-      form: {
-        _id: USER_RESOURCE_FORM_ID, // form.io form Id of user resource
-      },
-      user: {
-        _id: "anonymous", // keep it like that
-        roles: roles,
-      },
-    },  'JWT_SECRET'
-  );
-  // eslint-disable-next-line no-console
-  console.log(token)
-
   localStorage.setItem("vue-token", keycloak.token)
   localStorage.setItem("vue-refresh-token", keycloak.refreshToken)
-  localStorage.setItem("formio-token", token)
+
+  const email = keycloak.tokenParsed.email; 
+  // const USER_RESOURCE_FORM_ID = process.env.VUE_APP_USER_RESOURCE_FORM_ID;
+
+  const REVIEWER_ROLE_ID = process.env.VUE_APP_DESIGNER_ROLE;
+  const roles = [REVIEWER_ROLE_ID];
+  
+  console.log(email)
+  console.log(roles);
+  //  authenticateFormio(email, roles);
+
+
+  // const authenticateFormio = (user, roles) => {
+  //   const FORMIO_TOKEN = jwt.sign(
+  //     {
+  //       form: {
+  //         _id: USER_RESOURCE_FORM_ID, // form.io form Id of user resource
+  //       },
+  //       user: {
+  //         _id: user, // keep it like that
+  //         roles: roles,
+  //       },
+  //     },
+  //     "--- change me now ---"
+  //   ); // JWT secret key
+  //   //TODO remove this token from local Storage on logout and try to move to redux store as well
+  //   console.log(FORMIO_TOKEN);
+  //   localStorage.setItem("formioToken", FORMIO_TOKEN);
+  // };
 
   new Vue({
     router,
-    render: h => h(App, {props: {keycloak: keycloak, formio_token: token}}),
+    store,
+    render: h => h(App, {props: {keycloak: keycloak,}}),
   }).$mount('#app')
 
 
@@ -92,22 +120,3 @@ keycloak.init({ onLoad: initOptions.onLoad }).then((auth) =>{
 }).catch(() =>{
 Vue.$log.error("Authenticated Failed");
 });
-
-// work based on  https://medium.com/js-dojo/authentication-made-easy-in-vue-js-with-keycloak-c03c7fff67bb
-// Vue.$keycloak
-//     .init({
-//         onLoad: 'login-required',
-//     })
-//     .then((authenticated) => {
-//         new Vue({
-//             router,
-//             render: h => h(App),
-//         }).$mount('#app');
-//     });
-
-// new Vue({
-//   el: '#app',
-//   router,
-//   template: '<App/>',
-//   components: { App }
-// });

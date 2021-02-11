523b65bb4febf39375dd26240cee7b3372ff780b
kurianbenoy <kurian.benoy@aot-technologies.com>
2021-02-11 19:56:07 +0530

    fix missing css

--- queue-managment/src/main.ts
+++ queue-managment/src/main.ts
@@ -7,6 +7,9 @@ import jwt from 'jsonwebtoken'
 import Keycloak from 'keycloak-js';
 import {authenticateFormio} from "@/services/formio-token";
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue)

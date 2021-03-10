5c9c9d0ff6fa2c196e4be9d2f4b8667f5269f1ac
abhilash-aot <abhilash.kr@aot-technologies.com>
2021-03-10 18:21:04 +0530

    Updated Sorting

--- forms-flow-web/src/components/ServiceFlow/filter/TaskSort.js
+++ forms-flow-web/src/components/ServiceFlow/filter/TaskSort.js
@@ -1,15 +1,14 @@
-import {sortingList} from "../constants/taskConstants";
 import React from "react";
 
-const TaskSort = () => {
+const TaskSort = ({handleClick, options}) => {
 
-  const onSortSelect = () => {
-
+  const onSortSelect = (sortType) => {
+    handleClick(sortType);
   };
 
   return  (<div className="sort-items">
-    {sortingList.map(sortType => (
-        <div className="clickable p-0 mb-2" onClick={() => onSortSelect(sortType)}>
+    {options.map((sortType,index) => (
+        <div key={index} className="clickable p-0 mb-2" onClick={() => onSortSelect(sortType)}>
           {sortType.label}
         </div>
       )
--- forms-flow-web/src/components/ServiceFlow/filter/TaskSortSelectedList.js
+++ forms-flow-web/src/components/ServiceFlow/filter/TaskSortSelectedList.js
@@ -1,19 +1,50 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {sortingList} from "../constants/taskConstants";
 import TaskSort from "./TaskSort";
 
 const TaskSortSelectedList = () => {
-  const currentSortList =sortingList;
+  const createNode = useRef();
 
-  const [sortList,updateSortList] = useState(currentSortList);
+  const [sortList,updateSortList] = useState([]);
   const [showSortListDropdown,setShowSortListDropdown] = useState(false);
   const [showSortListDropdownIndex, setShowSortListDropdownIndex] = useState(null);
+
+  const handleClick = e => {
+    if (createNode.current.contains(e.target)) {
+      return;
+    };
+    // outside click
+    setShowSortListDropdown(null);
+    setShowSortListDropdownIndex(null);
+  };
+
+  const addSort = (sort)=>{
+    let updatedSortList = [...sortList];
+    updatedSortList.push({...sort});
+    updateSortList(updatedSortList);
+    setShowSortListDropdown(null);
+  }
+
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
+
+  useEffect(() => {
+    console.log("sortListChanged", sortList);
+  }, [sortList]);
 
   const updateSortOrder = (index,sortOrder)=>{
    let updatedSortList = [...sortList];
    updatedSortList[index].sortOrder=sortOrder;
-   updateSortList(updatedSortList)
+   updateSortList(updatedSortList);
   };
+
 
   const showSortList = (index)=>{
     if(index!==showSortListDropdownIndex){
@@ -23,12 +54,27 @@ const TaskSortSelectedList = () => {
     }
   }
 
- const selectedSortList = () => {
+  const updateSort = (sort,index)=>{
+    console.log("update",sortList, sort, index, sortingList);
+    let updatedSortList = [...sortList];
+    updatedSortList[index].label=sort.label;
+    updateSortList(updatedSortList)
+    setShowSortListDropdown(null);
+    setShowSortListDropdownIndex(null);
+  }
+
+  const deleteSort = (sort,index)=>{
+    let updatedSortList = [...sortList];
+    updatedSortList.splice(index,1);
+    updateSortList(updatedSortList)
+  }
+
+  const selectedSortList = () => {
    return sortList.map((sort, index)=>(
-     <div className="mr-2">
-       <span className="mr-1 font-weight-bold click-element" title="Remove sort">x</span>
+     <div className="mr-3" key={index}>
+       {sortList.length>1 ?<span className="mr-1 font-weight-bold click-element" title="Remove sorting" onClick={()=>deleteSort(sort,index)}>x</span>:null}
        <span className="mr-1 click-element" onClick={()=>showSortList(index)}>
-         {sort.label}{showSortListDropdownIndex===index?<TaskSort/>:null}
+         {sort.label}{showSortListDropdownIndex===index?<TaskSort handleClick={(sortToUpdate)=>updateSort(sortToUpdate,index)} options={sortingList}/>:null}
        </span>
        <span className="click-element">
          {sort.sortOrder==="asc"?<i className="fa fa-angle-up fa-lg font-weight-bold" title="Ascending" onClick={()=>updateSortOrder(index,"desc")}/>:
@@ -38,9 +84,12 @@ const TaskSortSelectedList = () => {
    ))
   };
 
-  return  (<div className="d-flex flex-wrap">
+  return  (<div className="d-flex flex-wrap" ref={createNode}>
     {selectedSortList()}
-    <div className="ml-1"> <i className="fa fa-plus fa-sm click-element" onClick={()=>setShowSortListDropdown(!showSortListDropdown)}/> {showSortListDropdown?<TaskSort/>:null}</div>
+    <div className="ml-1">
+      <i className="fa fa-plus fa-sm click-element" onClick={()=>setShowSortListDropdown(!showSortListDropdown)} title="Add sorting"/>
+     {showSortListDropdown?<TaskSort handleClick={addSort} options={sortingList}/>:null}
+    </div>
   </div>)
 };
 

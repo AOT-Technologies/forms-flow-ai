a2ef5ca830dfd75b0e38c820b422ed34c16a91d0
abhilash-aot <abhilash.kr@aot-technologies.com>
2021-03-16 16:06:26 +0530

    form grid updated for reviewer/cclient

--- forms-flow-web/src/apiManager/services/bpmServices.js
+++ forms-flow-web/src/apiManager/services/bpmServices.js
@@ -27,4 +27,26 @@ export const formatForms = (forms) => {
     return {"_id":form.formId , "title": form.formName, processKey:form.processKey}
   });
 }
+const dynamicSort = (property) => {
+  let sortOrder = 1;
+  if(property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return  (a,b)=> {
+    /* next line works with strings and numbers,
+     * and you may want to customize it to your needs
+     */
+    const result = (a[property].toUpperCase() < b[property].toUpperCase()) ? -1 : (a[property].toUpperCase() > b[property].toUpperCase()) ? 1 : 0;
+    return result * sortOrder;
+  }
+};
 
+export const getPaginatedForms = (forms, limit, page, sort) => {
+  if(sort){
+    forms.sort(dynamicSort(sort));
+    return forms.slice((page-1)*limit,((page-1)*limit)+limit);
+  }else {
+    return forms.slice((page-1)*limit,((page-1)*limit)+limit);
+  }
+}
--- forms-flow-web/src/modules/bpmFormReducer.js
+++ forms-flow-web/src/modules/bpmFormReducer.js
@@ -1,17 +1,44 @@
 import ACTION_CONSTANTS from "../actions/actionConstants";
-import {formatForms} from "../apiManager/services/bpmServices";
+import {formatForms, getPaginatedForms} from "../apiManager/services/bpmServices";
 
 const initialState = {
-  formList:[],
-  isFormLoading:false
-}
+  error: '',
+  formsFullList:[],
+  forms: [],
+  isActive: false,
+  limit:10,
+  pagination: {
+  numPages: 0,
+    page: 1,
+    total: 0,
+  },
+  query:{},
+  select:'',
+  sort:'',
+  totalForms:"",
+};
 
 const bpmForms = (state = initialState, action)=> {
   switch (action.type) {
     case ACTION_CONSTANTS.BPM_FORM_LIST:
-      return {...state, formList: formatForms(action.payload), isFormLoading: false};
+      return {...state, forms: getPaginatedForms(formatForms(action.payload),state.limit,1, state.sort), formsFullList:formatForms(action.payload), isActive: false,  totalForms:action.payload.length,  pagination: {
+          numPages: Math.ceil(action.payload.length/ state.limit),
+          page: 1,
+          total: action.payload.length,
+        }};
     case ACTION_CONSTANTS.IS_BPM_FORM_LIST_LOADING:
-      return {...state, isFormLoading: action.payload};
+      return {...state, isActive: action.payload};
+    case ACTION_CONSTANTS.BPM_FORM_LIST_PAGE_CHANGE:
+      return {...state, pagination: {...state.pagination,...{
+          page: action.payload}},forms:getPaginatedForms(state.formsFullList,state.limit,action.payload, state.sort)};
+    case ACTION_CONSTANTS.BPM_FORM_LIST_LIMIT_CHANGE:
+      return {...state, limit:action.payload, pagination: {
+          numPages: Math.ceil(state.totalForms/ action.payload),
+          page: 1,
+          total: state.totalForms,
+        }, forms:getPaginatedForms(state.formsFullList,action.payload,1, state.sort)}
+    case ACTION_CONSTANTS.BPM_FORM_LIST_SORT_CHANGE:
+      return {...state, sort:action.payload,forms:getPaginatedForms(state.formsFullList,state.limit,state.pagination.page, action.payload)};
     default:
       return state;
   }

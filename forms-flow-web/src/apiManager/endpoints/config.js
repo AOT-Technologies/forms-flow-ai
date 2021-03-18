22f83c690baf163c77768aa7f1ccf3fa1d0384f2
abhilash-aot <abhilash.kr@aot-technologies.com>
2021-03-18 13:05:37 +0530

    socket.io test (#278)

--- forms-flow-web/src/apiManager/endpoints/config.js
+++ forms-flow-web/src/apiManager/endpoints/config.js
@@ -11,3 +11,5 @@ export const BPM_BASE_URL = `${(window._env_ && window._env_.REACT_APP_CAMUNDA_A
 
 export const BPM_BASE_URL_EXT = `${(window._env_ && window._env_.REACT_APP_CAMUNDA_API_URI) || process.env.REACT_APP_CAMUNDA_API_URI}/engine-rest-ext`;
 // export const BPM_PROCESS_BASE_API = `${BPM_BASE_URL}/process-definition/key/`;
+
+export const BPM_BASE_URL_SOCKET_IO = `${(window._env_ && window._env_.REACT_APP_CAMUNDA_API_URI) || process.env.REACT_APP_CAMUNDA_API_URI}/forms-flow-bpm-socket`;

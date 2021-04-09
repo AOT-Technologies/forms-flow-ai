be97176541c4d3b55ffc003eabf23356bf9c3f9c
abhilash-aot <abhilash.kr@aot-technologies.com>
2021-04-09 12:06:48 +0530

    updated Task Filter

--- forms-flow-web/src/components/ServiceFlow/list/search/TaskFilterComponent.js
+++ forms-flow-web/src/components/ServiceFlow/list/search/TaskFilterComponent.js
@@ -1,25 +1,20 @@
 import React, {useEffect, useRef, useState} from "react";
 import TaskFilterDropdown from "./TaskFilterDropdown";
 import TaskFilterSearch from "./TaskFilterSearch";
-import Checkbox from "@material-ui/core/Checkbox";
 import {QUERY_TYPES} from "../../constants/taskConstants";
 import {
   setFilterListSearchParams,
-  setIsVariableNameIgnoreCase, setIsVariableValueIgnoreCase,
   setSearchQueryType
 } from "../../../../actions/bpmTaskActions";
 import {useDispatch, useSelector} from "react-redux";
-import {isVariableTypeAvailable} from "../../../../apiManager/services/taskSearchParamsFormatterService";
+import TaskIgnoreCaseComponent from "./TaskIgnoreCaseComponent";
 
 const TaskFilterComponent = ({totalTasks}) => {
   const createSearchNode = useRef();
   const filterSearchSelections = useSelector(state => state.bpmTasks.filterSearchSelections);
   const queryType = useSelector(state => state.bpmTasks.searchQueryType);
-  const variableNameIgnoreCase = useSelector(state => state.bpmTasks.variableNameIgnoreCase);
-  const variableValueIgnoreCase = useSelector(state => state.bpmTasks.variableValueIgnoreCase);
   const [filterSelections, setFilterSelections] = useState(filterSearchSelections);
   const [showFilterItems, setShowFilterItems] = useState(false);
-  const [isVariableTypeInFilter, setIsVariableTypeInFilter] = useState(false);
   const dispatch= useDispatch();
 
 
@@ -40,10 +35,6 @@ const TaskFilterComponent = ({totalTasks}) => {
       document.removeEventListener("mousedown", handleClick);
     };
   }, []);
-
-  useEffect(()=>{
-    setIsVariableTypeInFilter(isVariableTypeAvailable(filterSelections));
-  },[filterSelections]);
 
   useEffect(()=>{
     dispatch(setFilterListSearchParams(filterSelections));
@@ -81,14 +72,6 @@ const TaskFilterComponent = ({totalTasks}) => {
     dispatch(setSearchQueryType(type));
   }
 
-  const setVariableNameIgnoreCase=(isIgnoreCase)=>{
-    dispatch(setIsVariableNameIgnoreCase(isIgnoreCase))
-  }
-
-  const setVariableValueIgnoreCase = (isIgnoreCase)=>{
-    dispatch(setIsVariableValueIgnoreCase(isIgnoreCase))
-  }
-
   const changeQueryType= () => {
     queryType===QUERY_TYPES.ALL? setQueryType(QUERY_TYPES.ANY):setQueryType(QUERY_TYPES.ALL);
   };
@@ -121,29 +104,8 @@ const TaskFilterComponent = ({totalTasks}) => {
             ) : null}
             <span dat-title="Total number of results">{totalTasks}</span>
           </div>
-
-
         </div>
-
-        {filterSelections.length && isVariableTypeInFilter? <div>
-          <span className="name-value-container">For Variables, ignore case of
-            <Checkbox
-              className="check-box-design"
-              checked={variableNameIgnoreCase}
-              onChange={()=>setVariableNameIgnoreCase(!variableNameIgnoreCase)}
-              inputProps={{'aria-label': 'primary checkbox'}}
-            />
-            name
-            <Checkbox
-              className="check-box-design"
-              checked={variableValueIgnoreCase}
-              onChange={()=>setVariableValueIgnoreCase(!variableValueIgnoreCase)}
-              inputProps={{'aria-label': 'primary checkbox'}}
-            />  value.
-          </span>
-          {/*<span className="filter-action-container">  <i className="fa fa-link item-pos" aria-hidden="true"/><i
-            className="fa fa-floppy-o item-pos" aria-hidden="true"/></span>*/}
-        </div> : null}
+        <TaskIgnoreCaseComponent/>
       </div>
     </>
   );

f22a726bb8d1c3e26b005f34c9c4c4ff37b3a4ef
abhilash-aot <abhilash.kr@aot-technologies.com>
2021-04-09 15:44:51 +0530

    Search query updates

--- forms-flow-web/src/components/ServiceFlow/constants/taskConstants.js
+++ forms-flow-web/src/components/ServiceFlow/constants/taskConstants.js
@@ -28,28 +28,36 @@ export const Filter_Search_Types = {
   DATE:"date",
   NORMAL:"normal"
 }
+
+
+export const FILTER_OPERATOR_TYPES = {
+  EQUAL:"=",
+  LIKE:"like",
+  BEFORE:"before",
+  AFTER:"after"
+}
+
 //TODO update to further constants
 export const FILTER_COMPARE_OPTIONS = {
-  [Filter_Search_Types.VARIABLES]:[">", ">=", "=","!=", "<", "<="],
-  [Filter_Search_Types.DATE]:["before", "after"],
-  [Filter_Search_Types.STRING]:["=","like"],
-  [Filter_Search_Types.NORMAL]:["="]
+  [Filter_Search_Types.VARIABLES]:[">", ">=", FILTER_OPERATOR_TYPES.EQUAL ,"!=", "<", "<=",FILTER_OPERATOR_TYPES.LIKE],
+  [Filter_Search_Types.DATE]:[FILTER_OPERATOR_TYPES.BEFORE, FILTER_OPERATOR_TYPES.AFTER],
+  [Filter_Search_Types.STRING]:[FILTER_OPERATOR_TYPES.EQUAL,FILTER_OPERATOR_TYPES.LIKE],
+  [Filter_Search_Types.NORMAL]:[FILTER_OPERATOR_TYPES.EQUAL]
 };
 
-
 export const taskFilters = [
-  {label:"Process Variables",key:"processVariables", operator:"=", type:Filter_Search_Types.VARIABLES, value:"", name:""},
-  {label:"Task Variables", key:"taskVariables",operator:"=", type:Filter_Search_Types.VARIABLES, value:"", name:""},
-  {label:"Process Definition Name",key:"processDefinitionName", operator:"like",type:Filter_Search_Types.STRING, value:"" },
-  {label:"Assignee",key:"assignee",operator:"like", type:Filter_Search_Types.STRING,value:"", },
-  {label:"Candidate Group",key:"candidateGroup",operator:"=",type:Filter_Search_Types.NORMAL, value:""},
-  {label:"Candidate User",key:"candidateUser",operator:"=",type:Filter_Search_Types.NORMAL, value:""},
-  {label:"Name",key:"name",operator:"like",type:Filter_Search_Types.STRING,value:""},
-  {label:"Description",key:"description",operator:"like",type:Filter_Search_Types.STRING, value:""},
-  {label:"Priority",key:"priority",operator:"=",type:Filter_Search_Types.NORMAL, value:""},
-  {label:"Due Date",key:"due",operator:"before", type:Filter_Search_Types.DATE, value:""},
-  {label:"Follow up Date",key:"followUp",operator:"before", type:Filter_Search_Types.DATE, value:""},
-  {label:"Created",key:"created",operator:"before",type:Filter_Search_Types.DATE, value:"" },
+  {label:"Process Variables",key:"processVariables", operator:FILTER_OPERATOR_TYPES.EQUAL, type:Filter_Search_Types.VARIABLES, value:"", name:""},
+  {label:"Task Variables", key:"taskVariables",operator:FILTER_OPERATOR_TYPES.EQUAL, type:Filter_Search_Types.VARIABLES, value:"", name:""},
+  {label:"Process Definition Name",key:"processDefinitionName", operator:FILTER_OPERATOR_TYPES.LIKE, type:Filter_Search_Types.STRING, value:"" },
+  {label:"Assignee",key:"assignee",operator:FILTER_OPERATOR_TYPES.LIKE, type:Filter_Search_Types.STRING,value:"", },
+  {label:"Candidate Group",key:"candidateGroup",operator:FILTER_OPERATOR_TYPES.EQUAL,type:Filter_Search_Types.NORMAL, value:""},
+  {label:"Candidate User",key:"candidateUser",operator:FILTER_OPERATOR_TYPES.EQUAL,type:Filter_Search_Types.NORMAL, value:""},
+  {label:"Name",key:"name",operator:FILTER_OPERATOR_TYPES.LIKE,type:Filter_Search_Types.STRING,value:""},
+  {label:"Description",key:"description",operator:FILTER_OPERATOR_TYPES.LIKE,type:Filter_Search_Types.STRING, value:""},
+  {label:"Priority",key:"priority",operator:FILTER_OPERATOR_TYPES.EQUAL,type:Filter_Search_Types.NORMAL, value:""},
+  {label:"Due Date",key:"due",operator:FILTER_OPERATOR_TYPES.BEFORE, type:Filter_Search_Types.DATE, value:""},
+  {label:"Follow up Date",key:"followUp",operator:FILTER_OPERATOR_TYPES.BEFORE, type:Filter_Search_Types.DATE, value:""},
+  {label:"Created",key:"created",operator:FILTER_OPERATOR_TYPES.BEFORE,type:Filter_Search_Types.DATE, value:"" },
 ];
 
 export const ALL_TASKS="All tasks"

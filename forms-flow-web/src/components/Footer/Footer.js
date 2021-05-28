3b65a9a2bd310c5bcb8047f5deea1e58bbe142a1
Abil P Raju <83952803+abilpraju-aot@users.noreply.github.com>
2021-05-28 17:05:45 +0530

    updated footer and images

--- forms-flow-web/src/components/Footer/Footer.js
+++ forms-flow-web/src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.scss";
 const Footer = React.memo(() => {
-  const today = new Date();
+  //const today = new Date();
   return (
     <div className="row footer">
       <div className="col-12 text-center text-align">

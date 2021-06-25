6627d37ee5b3569e277b1c042552dad7eae6e2da
Abil P Raju <83952803+abilpraju-aot@users.noreply.github.com>
2021-06-25 22:01:24 +0530

    versioning done (#398)
    * versioning done
    
    * updated version name using env variable

--- forms-flow-web/src/components/Footer/Footer.js
+++ forms-flow-web/src/components/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./footer.scss";
+import { APPLICATION_NAME } from "../../constants/constants";
+import {version} from "../../../package.json"
+const appname = APPLICATION_NAME;
+const version_org = version;
 const Footer = React.memo(() => {
   //const today = new Date();
   return (
     <div className="row footer">
       <div className="col-12 text-center text-align">
-      Powered by<a href="https://formsflow.ai" target='_blank'  rel="noreferrer"> formsflow.ai</a> {/*{today.getFullYear()}*/}
+      Powered by<a href="https://formsflow.ai" target='_blank'  rel="noreferrer"> {appname}</a> v{version_org} {/*{today.getFullYear()}*/}
       </div>
     </div>
   );

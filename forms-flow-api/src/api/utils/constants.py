9853eba208a5ea00517efc67c43b7cfba49303ac
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-07-06 21:22:21 +0530

    Resolve conflicts

--- forms-flow-api/src/api/utils/constants.py
+++ forms-flow-api/src/api/utils/constants.py
@@ -1,14 +1,9 @@
 import os
 
 FORMSFLOW_API_CORS_ORIGINS = os.getenv("FORMSFLOW_API_CORS_ORIGINS")
-<<<<<<< HEAD
 if FORMSFLOW_API_CORS_ORIGINS == "*":
     CORS_ORIGINS = "*"
 else:
-=======
-if FORMSFLOW_API_CORS_ORIGINS=="*": CORS_ORIGINS = "*"
-else: 
->>>>>>> d6193976573a24f5bb6f02b70b1dbe2e246f0a3d
     CORS_ORIGINS = FORMSFLOW_API_CORS_ORIGINS.split(",")
 REVIEWER_GROUP = "formsflow-reviewer"
 ALLOW_ALL_APPLICATIONS = "/formsflow/formsflow-reviewer/access-allow-applications"

580d027b21b1219a0695e3b5cee35af84a94161e
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-07-19 12:40:35 +0530

    add training job file

--- forms-flow-data-analysis-api/src/api/models/__init__.py
+++ forms-flow-data-analysis-api/src/api/models/__init__.py
@@ -1,4 +1,4 @@
 """This exports all of the models used by the application."""
 
-from models.pg.db import db, ma
-from models.mongo.db import mongo
+from api.models.pg.db import db, ma
+from api.models.mongo.db import mongo

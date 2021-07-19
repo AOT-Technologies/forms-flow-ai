f63ac2fb0e2f03d6f17953bd77f57b4f5ca4c403
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-07-19 17:52:44 +0530

    remove forms-flow-sentiment folder

--- forms-flow-sentiment/src/sentiment-api/resources/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_restx import Api
-
-sentiment_api = Api(
-    version='1.0',
-    title='Sentiment-API',
-    description='API endpoint for sentiment analysis component'
-)
-
-sentiment_api.add_namespace(sentiment_namespace, path='/sentiment')

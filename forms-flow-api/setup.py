5f7f31da9cb2098d2b67084182073b8a1aa816e2
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-07-19 15:33:22 +0530

    remove sentiment analysis component from webapi

--- forms-flow-api/setup.py
+++ forms-flow-api/setup.py
@@ -38,7 +38,6 @@ setup(
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
-    license=read('LICENSE'),
     long_description=read('README.md'),
     zip_safe=False,
     install_requires=REQUIREMENTS,

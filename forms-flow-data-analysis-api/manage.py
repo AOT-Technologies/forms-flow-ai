f9f6961165bf42b5a27e6a654213d4e42dddb2ca
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-08-02 12:51:44 +0530

    temp

--- forms-flow-data-analysis-api/manage.py
+++ forms-flow-data-analysis-api/manage.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
 
 # models included so that migrate can build the database migrations
 from api import create_app
@@ -11,8 +11,10 @@ from api.models import db
 
 APP = create_app()
 MIGRATE = Migrate(APP, db)
+MANAGER = Manager(APP)
 
+MANAGER.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     logging.log(logging.INFO, 'Running the Manager')
-    APP.run()
+    MANAGER.run()

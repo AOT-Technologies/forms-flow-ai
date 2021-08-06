08107d1860aed2e369261f85d1fa4a2fe0861207
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-08-06 21:50:14 +0530

    add Dockerfiles

--- forms-flow-data-analysis-api/manage.py
+++ forms-flow-data-analysis-api/manage.py
@@ -1,12 +1,7 @@
 """Manage the database and some other items required to run the API."""
 
 import logging
-
-from flask_migrate import Migrate
-
-# models included so that migrate can build the database migrations
 from api import create_app
-
 
 APP = create_app()
 

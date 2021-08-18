e90fe0f654d5b46af894a7d4be5a7a57e059e06f
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-08-18 21:16:55 +0530

    update data analysis API

--- forms-flow-data-analysis-api/tests/unit/api/test_sentiment_analysis.py
+++ forms-flow-data-analysis-api/tests/unit/api/test_sentiment_analysis.py
@@ -2,3 +2,6 @@ def test_sentiment_analysis_api_without_bearer_token(client):
     response = client.post('/sentiment')
     assert response.status_code == 401
     assert response.json == {"message": "Access Denied"}
+
+def test_sentiment_analysis_api(session, client):
+    pass

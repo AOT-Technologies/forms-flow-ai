00da9f5a7cf4fc60e738a468dff0360dda95a03b
Kurian Benoy <70306694+kurianbenoy-aot@users.noreply.github.com>
2021-12-27 12:04:46 +0530

    Updates/api & Unit Testing standardisation (#650)
    * Update groups.py
    
    * update API
    
    * comment out unused API in process management
    
    * update API
    
    * remove unused code
    
    * update process schemas
    
    * update testing
    
    * update tests header
    
    * Update config.py

--- forms-flow-api/src/formsflow_api/resources/process.py
+++ forms-flow-api/src/formsflow_api/resources/process.py
@@ -6,44 +6,10 @@ from flask import current_app, request
 from flask_restx import Namespace, Resource
 
 from formsflow_api.services import ProcessService
-
-from formsflow_api.schemas.process import ProcessMessageSchema
 from formsflow_api.utils import auth, cors_preflight, profiletime
 
 
 API = Namespace("Process", description="Process")
-
-
-# @cors_preflight("GET,OPTIONS")
-# @API.route(
-#     "/<string:process_key>/task/<string:task_key>/state", methods=["GET", "OPTIONS"]
-# )
-# class ProcessStateResource(Resource):
-#     """Resource for managing state."""
-
-#     @staticmethod
-#     @auth.require
-#     @profiletime
-#     def get(process_key, task_key):
-#         """Get states by process and task key."""
-#         try:
-#             return (
-#                 (
-#                     ProcessService.get_states(
-#                         process_key, task_key, request.headers["Authorization"]
-#                     )
-#                 ),
-#                 HTTPStatus.OK,
-#             )
-#         except BaseException as err:
-#             response, status = {
-#                 "type": "Bad request error",
-#                 "message": "Invalid request data object",
-#             }, HTTPStatus.BAD_REQUEST
-
-#             current_app.logger.warning(response)
-#             current_app.logger.warning(err)
-#             return response, status
 
 
 @cors_preflight("GET,OPTIONS")
@@ -105,79 +71,3 @@ class ProcessDefinitionResource(Resource):
             current_app.logger.warning(response)
             current_app.logger.warning(err)
             return response, status
-
-
-# @cors_preflight("POST,OPTIONS")
-# @API.route("/event", methods=["POST", "OPTIONS"])
-# class ProcessEventResource(Resource):
-#     """Resource for managing state."""
-
-#     @staticmethod
-#     @auth.require
-#     @profiletime
-#     def post():
-#         message_json = request.get_json()
-#         message_schema = ProcessMessageSchema()
-#         dict_data = message_schema.load(message_json)
-#         """Get states by process and task key."""
-#         try:
-#             return (
-#                 (
-#                     ProcessService.post_message(
-#                         dict_data, request.headers["Authorization"]
-#                     )
-#                 ),
-#                 HTTPStatus.OK,
-#             )
-#         except KeyError as err:
-#             response, status = (
-#                 {
-#                     "type": "Invalid Request Object",
-#                     "message": "Required fields are not passed",
-#                     "errors": err.messages,
-#                 },
-#                 HTTPStatus.BAD_REQUEST,
-#             )
-
-#             current_app.logger.critical(response)
-#             current_app.logger.critical(err)
-#             return response, status
-#         except BaseException as err:
-#             response, status = {
-#                 "type": "Bad request error",
-#                 "message": "Invalid request data object",
-#             }
-
-#             current_app.logger.warning(response)
-#             current_app.logger.warning(err)
-#             return response, status
-
-
-# @cors_preflight("GET,OPTIONS")
-# @API.route(
-#     "/process-instance/<string:process_InstanceId>/activity-instances",
-#     methods=["GET", "OPTIONS"],
-# )
-# class ProcessInstanceResource(Resource):
-#     """Get Process Activity Instances."""
-
-#     @staticmethod
-#     @auth.require
-#     @profiletime
-#     def get(process_InstanceId):
-#         """Get states by process and task key."""
-#         try:
-#             return (
-#                 ProcessService.get_process_activity_instances(
-#                     process_InstanceId, request.headers["Authorization"]
-#                 ),
-#                 HTTPStatus.OK,
-#             )
-#         except BaseException as err:
-#             response, status = {
-#                 "type": "Bad request error",
-#                 "message": "Invalid request data object",
-#             }, HTTPStatus.BAD_REQUEST
-
-#             current_app.logger.warning(response)
-#             return response, status

15464a963c3b12941fdce23d7da07af2af5071f0
shibin-aot <shibin.thomas@aot-technologies.com>
2022-03-25 12:47:12 +0530

    logger

--- forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/controllers/AdminController.java
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/controllers/AdminController.java
@@ -62,7 +62,7 @@ public class AdminController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         List<String> groups = getGroups(authentication);
         AuthorizationInfo authorizationInfo = null;
-        LOGGER.error(groups.toString() + " "+((JwtAuthenticationToken)authentication).getToken());
+        LOGGER.error(groups.toString() + " "+((JwtAuthenticationToken)authentication).getToken().getTokenValue());
         if (CollectionUtils.isNotEmpty(groups) && groups.contains(adminGroupName)) {
             authorizationInfo = new AuthorizationInfo(true, null);
         } else {

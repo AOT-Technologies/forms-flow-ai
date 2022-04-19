e8cb92a130704513ba8bac029f96c55acab571d0
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2022-04-19 12:58:45 -0700

    Adding tenant keycloak client

--- forms-flow-web/src/helper/helper.js
+++ forms-flow-web/src/helper/helper.js
@@ -2,4 +2,10 @@ const replaceUrl = (URL, key, value) => {
   return URL.replace(key, value);
 };
 
-export { replaceUrl };
+const getUrlParamValue = (name) => {
+  if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(window.location.search))
+     return decodeURIComponent(name[1]);
+}
+
+
+export { replaceUrl, getUrlParamValue };

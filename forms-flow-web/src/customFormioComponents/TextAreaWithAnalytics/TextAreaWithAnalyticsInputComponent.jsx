77544e4765b009ad4270951b57469f282df7717e
abhilash-aot <abhilash.kr@aot-technologies.com>
2022-04-27 15:48:14 +0530

    removed Hidden Element

--- forms-flow-web/src/customFormioComponents/TextAreaWithAnalytics/TextAreaWithAnalyticsInputComponent.jsx
+++ forms-flow-web/src/customFormioComponents/TextAreaWithAnalytics/TextAreaWithAnalyticsInputComponent.jsx
@@ -18,9 +18,7 @@ export default class SentimentAnalytics extends Component {
     const { disabled, name } = this.props;
     let { value } = this.state;
     const text = value?.text || '';
-    const SentiAnalysisValue = value?.overallSentiAnalysis || '';
     return (
-      /*<input type="text" value={value} className={this.props.component.customClassName} onChange={this.setValue}></input>*/
       <>
         <textarea
           name={name}
@@ -30,16 +28,6 @@ export default class SentimentAnalytics extends Component {
           onChange={this.updateCommentData}
           disabled={disabled}
         />
-        <text
-          //This component will show the overall result of sentiment analysis
-          hidden
-          style={{ marginTop: '5px' }}
-          name={name}
-          disabled
-          value={SentiAnalysisValue}
-          className="form-control"
-        />
-
       </>
     );
   }

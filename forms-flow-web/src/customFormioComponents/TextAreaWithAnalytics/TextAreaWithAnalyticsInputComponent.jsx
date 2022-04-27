0210d9e415dfecc387a201c63d8f33b0bcd746bf
DESKTOP-EGTI4MU\DELL <alex.antony@aot-technology.com>
2022-04-27 14:40:04 +0530

    Sentiment analysis overall (custom component)

--- forms-flow-web/src/customFormioComponents/TextAreaWithAnalytics/TextAreaWithAnalyticsInputComponent.jsx
+++ forms-flow-web/src/customFormioComponents/TextAreaWithAnalytics/TextAreaWithAnalyticsInputComponent.jsx
@@ -1,8 +1,6 @@
-import React, {Component} from 'react';
-/**
- * An example React component this is simply a controlled input element.
- *
- */
+import React, { Component } from 'react';
+
+
 export default class SentimentAnalytics extends Component {
   constructor(props) {
     super(props);
@@ -12,24 +10,37 @@ export default class SentimentAnalytics extends Component {
   }
 
   updateCommentData = (event) => {
-    const {type, sentimentAnalyticTopics} = this.props.component;
-    this.setState({value: {text:event.target.value,type,topics:sentimentAnalyticTopics}}, () => this.props.onChange(this.state.value));
+    const { type, sentimentAnalyticTopics } = this.props.component;
+    this.setState({ value: { text: event.target.value, type, topics: sentimentAnalyticTopics, overallSentiment: '' } }, () => this.props.onChange(this.state.value));
   };
 
   render() {
-    const {disabled, name} = this.props;
+    const { disabled, name } = this.props;
     let { value } = this.state;
     const text = value?.text || '';
-    return  (
+    const SentiAnalysisValue = value?.overallSentiAnalysis || '';
+    return (
       /*<input type="text" value={value} className={this.props.component.customClassName} onChange={this.setValue}></input>*/
-      <textarea
-        name={name}
-        rows="3"
-        value={text}
-        className="form-control"
-        onChange={this.updateCommentData}
-        disabled={disabled}
-      />
+      <>
+        <textarea
+          name={name}
+          rows="3"
+          value={text}
+          className="form-control"
+          onChange={this.updateCommentData}
+          disabled={disabled}
+        />
+        <text
+          //This component will show the overall result of sentiment analysis
+          hidden
+          style={{ marginTop: '5px' }}
+          name={name}
+          disabled
+          value={SentiAnalysisValue}
+          className="form-control"
+        />
+
+      </>
     );
   }
 };

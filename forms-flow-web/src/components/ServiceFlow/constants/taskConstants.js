6052df6ca3e5e0e4d50f1b69062bfd3fed1d2e28
abhilash-aot <abhilash.kr@aot-technologies.com>
2022-04-28 12:00:10 +0530

    removed commented Code

--- forms-flow-web/src/components/ServiceFlow/constants/taskConstants.js
+++ forms-flow-web/src/components/ServiceFlow/constants/taskConstants.js
@@ -7,14 +7,6 @@ export const sortingList = [
   {sortBy:"name",label:<Translation>{(t)=>t("Task name")}</Translation>, sortOrder:"asc"},
   {sortBy:"followUpDate",label:<Translation>{(t)=>t("Follow up Date")}</Translation>, sortOrder:"asc"},
 ];
-// export const sortingList = [
-//   {sortBy:"created",label:"Created", sortOrder:"asc"},
-//   {sortBy:"priority",label:"Priority", sortOrder:"asc"},
-//   {sortBy:"dueDate",label:"Due date", sortOrder:"asc"},
-//   {sortBy:"assignee",label:"Assignee", sortOrder:"asc"},
-//   {sortBy:"name",label:"Task name", sortOrder:"asc"},
-//   {sortBy:"followUpDate",label:"Follow-up date", sortOrder:"asc"},
-// ];
 
 export const searchData = [
   {"label": <Translation>{(t)=>t("task_variable")}</Translation>, "compares": [">", ">=", "=","!=", "<", "<="]},

913450f7a1d3e4d3204f66a9f7498c02ae4bb3bd
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2022-05-05 13:47:10 -0700

    Changes for analytics per tenant

--- forms-flow-api/src/formsflow_api/services/external/analytics_api.py
+++ forms-flow-api/src/formsflow_api/services/external/analytics_api.py
@@ -3,19 +3,25 @@ from http import HTTPStatus
 
 import requests
 from flask import current_app
+from formsflow_api.utils.user_context import user_context, UserContext
 
 
 class RedashAPIService:  # pylint: disable=too-few-public-methods
     """This class manages all the Redash analytics service API calls."""
 
     @staticmethod
-    def get_request(url_path, page_no=None, limit=None):
+    @user_context
+    def get_request(url_path, page_no=None, limit=None, **kwargs):
         """This method makes the GET request to Redash API."""
+        user: UserContext = kwargs["user"]
+        base_url = f"{current_app.config.get('ANALYTICS_API_URL')}"
+        if tenant_key := user.tenant_key:
+            base_url = f"{base_url}/{tenant_key}"
         if page_no is None:
-            url = f"{current_app.config.get('ANALYTICS_API_URL')}/api/{url_path}"
+            url = f"{base_url}/api/{url_path}"
         else:
             url = (
-                f"{current_app.config.get('ANALYTICS_API_URL')}"
+                f"{base_url}"
                 f"/api/{url_path}?page={page_no}&page_size={limit}"
             )
         analytics_admin_token = current_app.config.get("ANALYTICS_API_KEY")

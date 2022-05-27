fd1103d3c75f3d5d6b126818616f7b644dd15a9a
sreehari ps <sreehari.ps@aot-technologies.com>
2022-05-27 15:21:04 +0530

    solves FWF-1445

--- forms-flow-web/src/components/Form/Item/Submission/index.js
+++ forms-flow-web/src/components/Form/Item/Submission/index.js
@@ -1,27 +1,26 @@
-import {Redirect, Route, Switch} from 'react-router-dom'
+import { Route, Switch} from 'react-router-dom'
 import React from 'react'
-import {useSelector} from "react-redux";
 
-import List from './List'
+// import List from './List'
 import Item from './Item/index'
-import {BASE_ROUTE, MULTITENANCY_ENABLED, STAFF_REVIEWER} from "../../../../constants/constants";
+import {BASE_ROUTE} from "../../../../constants/constants";
 
 
 const Form = React.memo(() => {
-  const userRoles= useSelector((state) => state.user.roles || []);
-  const showViewSubmissions= useSelector((state) => state.user.showViewSubmissions);
-  const tenantKey = useSelector((state) => state.tenants?.tenantId)
-  const redirectUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : `/`
-  const ViewSubmissionRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={(props) => (
-      userRoles.includes(STAFF_REVIEWER)
-        ? <Component {...props} />
-        : <Redirect exact to={redirectUrl} />
-    )} />
-  );
+  // const userRoles= useSelector((state) => state.user.roles || []);
+  // const showViewSubmissions= useSelector((state) => state.user.showViewSubmissions);
+  // const tenantKey = useSelector((state) => state.tenants?.tenantId)
+  // const redirectUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : `/`
+  // const ViewSubmissionRoute = ({ component: Component, ...rest }) => (
+  //   <Route {...rest} render={(props) => (
+  //     userRoles.includes(STAFF_REVIEWER)
+  //       ? <Component {...props} />
+  //       : <Redirect exact to={redirectUrl} />
+  //   )} />
+  // );
    return (<div>
       <Switch>
-        {showViewSubmissions?<ViewSubmissionRoute exact path={`${BASE_ROUTE}form/:formId/submission`} component={List} />:null}
+        {/* {showViewSubmissions?<ViewSubmissionRoute exact path={`${BASE_ROUTE}form/:formId/submission`} component={List} />:null} */}
         <Route path={`${BASE_ROUTE}form/:formId/submission/:submissionId`} component={Item} />
       </Switch>
     </div>)

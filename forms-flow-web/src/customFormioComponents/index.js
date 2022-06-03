4866dfe477c1ace42c9e4b2c58ded03ebdfec2fb
DESKTOP-EGTI4MU\DELL <alex.antony@aot-technology.com>
2022-06-03 15:01:24 +0530

    add cmis custom component

--- forms-flow-web/src/customFormioComponents/index.js
+++ forms-flow-web/src/customFormioComponents/index.js
@@ -1,7 +1,8 @@
 import TextAreaWithAnalytics from "./TextAreaWithAnalytics/TextAreaWithAnalytics";
-
+import FileUpload from "./FileUpload/fileUpload";
 const components = {
   textAreaWithAnalytics: TextAreaWithAnalytics,
+  fileUpload: FileUpload
 };
 
 export default components;

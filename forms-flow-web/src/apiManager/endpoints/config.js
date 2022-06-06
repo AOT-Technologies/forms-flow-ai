a0e40d4b964083060e43402ac1b0ab4ab5ee0914
sreehari ps <sreehari.ps@aot-technologies.com>
2022-06-06 19:54:38 +0530

    added api version as configurable

--- forms-flow-web/src/apiManager/endpoints/config.js
+++ forms-flow-web/src/apiManager/endpoints/config.js
@@ -29,3 +29,10 @@ export const MT_ADMIN_BASE_URL = `${
   (window._env_ && window._env_.REACT_APP_MT_ADMIN_BASE_URL) ||
   process.env.REACT_APP_MT_ADMIN_BASE_URL
 }`;
+
+
+export const MT_ADMIN_BASE_URL_VERSION = `${ 
+  (window._env_ && window._env_.REACT_APP_MT_ADMIN_BASE_URL_VERSION) ||
+  process.env.REACT_APP_MT_ADMIN_BASE_URL_VERSION ||
+  "v1"
+}`;

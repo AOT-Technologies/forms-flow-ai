66d9d365c404f08d3872d1e45429504583bdd04e
sreehari ps <sreehari.ps@aot-technologies.com>
2022-06-06 19:56:09 +0530

    formatted

--- forms-flow-web/src/apiManager/endpoints/config.js
+++ forms-flow-web/src/apiManager/endpoints/config.js
@@ -30,8 +30,7 @@ export const MT_ADMIN_BASE_URL = `${
   process.env.REACT_APP_MT_ADMIN_BASE_URL
 }`;
 
-
-export const MT_ADMIN_BASE_URL_VERSION = `${ 
+export const MT_ADMIN_BASE_URL_VERSION = `${
   (window._env_ && window._env_.REACT_APP_MT_ADMIN_BASE_URL_VERSION) ||
   process.env.REACT_APP_MT_ADMIN_BASE_URL_VERSION ||
   "v1"

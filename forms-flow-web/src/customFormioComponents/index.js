e40901646c54c911e3c36b03aacdeb6673662ae9
DESKTOP-EGTI4MU\DELL <alex.antony@aot-technology.com>
2022-06-21 08:29:57 +0530

    new google map custom component

--- forms-flow-web/src/customFormioComponents/index.js
+++ forms-flow-web/src/customFormioComponents/index.js
@@ -1,7 +1,10 @@
 import TextAreaWithAnalytics from "./TextAreaWithAnalytics/TextAreaWithAnalytics";
+import Maps from './Google-Map/GMapservice';
 
 const components = {
   textAreaWithAnalytics: TextAreaWithAnalytics,
+  Maps: Maps,
 };
 
 export default components;
+

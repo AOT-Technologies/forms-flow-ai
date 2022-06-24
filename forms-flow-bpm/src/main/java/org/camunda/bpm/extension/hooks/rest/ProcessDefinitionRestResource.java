aa0c5d9ffcf7a78f9c4667f30e5061b976f49383
shibin-aot <shibin.thomas@aot-technologies.com>
2022-06-24 15:50:34 +0530

    process definition start API moved to ext API set

--- forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/rest/ProcessDefinitionRestResource.java
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/rest/ProcessDefinitionRestResource.java
@@ -17,7 +17,7 @@ public interface ProcessDefinitionRestResource extends RestResource{
     @GetMapping(produces = MediaType.APPLICATION_JSON)
     CollectionModel<ProcessDefinitionDto> getProcessDefinition(@RequestParam  Map<String, Object> parameters);
 
-    @PostMapping(value = "/start/key/{key}", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    @PostMapping(value = "/key/{key}/start", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
     EntityModel<ProcessInstanceDto> startProcessInstanceByKey(
             @RequestParam Map<String, Object> parameters, @RequestBody StartProcessInstanceDto dto,
             @PathVariable("key") String key);

933a5ea9e4973bc0481152a8e0b7bedc2ab0245f
ratheesh-aot <ratheesh.kumar@aot-technologies.com>
2022-07-07 11:48:45 +0530

    updated chrome driver path

--- forms-flow-api/src/formsflow_api/utils/pdf.py
+++ forms-flow-api/src/formsflow_api/utils/pdf.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from seleniumwire import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from formsflow_api.utils import CHROME_DRIVER_PATH
 
 
 def send_devtools(driver, cmd, params=None):
@@ -28,7 +28,7 @@ def send_devtools(driver, cmd, params=None):
 
 def driver_path():
     """Return chrome webreiver path."""
-    return ChromeDriverManager().install()
+    return CHROME_DRIVER_PATH
 
 
 # pylint: disable=R1710

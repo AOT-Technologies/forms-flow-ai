671235e289cf39a96d02807bac6e20b3fdf4da78
ratheesh-aot <ratheesh.kumar@aot-technologies.com>
2022-07-11 15:27:15 +0530

    Custom component disabled

--- forms-flow-api/src/formsflow_api/static/js/from_io_render.js
+++ forms-flow-api/src/formsflow_api/static/js/from_io_render.js
@@ -2,11 +2,6 @@ function renderForm() {
     Formio.setBaseUrl(form_info.base_url);
     Formio.setProjectUrl(form_info.project_url);
     Formio.setToken(form_info.token)
-    Formio.use({
-      components: {
-        textAreaWithAnalytics: TextAreaWithAnalytics
-      }
-    });
     Formio.createForm(
         document.getElementById('formio'),
         form_info.form_url,

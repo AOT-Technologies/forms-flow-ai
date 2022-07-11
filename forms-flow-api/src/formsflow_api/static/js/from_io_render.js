93a304491c2f90ba9a0716f5fe1471dc1f629824
ratheesh-aot <ratheesh.kumar@aot-technologies.com>
2022-07-11 15:22:09 +0530

    Waiting until form is completely ready

--- forms-flow-api/src/formsflow_api/static/js/from_io_render.js
+++ forms-flow-api/src/formsflow_api/static/js/from_io_render.js
@@ -2,12 +2,19 @@ function renderForm() {
     Formio.setBaseUrl(form_info.base_url);
     Formio.setProjectUrl(form_info.project_url);
     Formio.setToken(form_info.token)
+    Formio.use({
+      components: {
+        textAreaWithAnalytics: TextAreaWithAnalytics
+      }
+    });
     Formio.createForm(
         document.getElementById('formio'),
         form_info.form_url,
         {readOnly : true, renderMode: 'flat'}
       ).then((form) => {
-        document.getElementById('formio').classList.add("completed")
+        form.ready.then(() => {
+          document.getElementById('formio').classList.add("completed")
+      });
       });
   }
   

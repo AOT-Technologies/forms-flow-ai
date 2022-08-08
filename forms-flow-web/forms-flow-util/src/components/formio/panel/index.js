50d35413cec3300e23475b6a5c4fb8c81b1705d0
Abil P Raju <83952803+abilpraju-aot@users.noreply.github.com>
2022-08-08 16:09:51 +0530

    updated vulnerability fix

--- forms-flow-web/forms-flow-util/src/components/formio/panel/index.js
+++
@@ -1,9 +0,0 @@
-import ContainerComponent from '../container';
-
-class PanelComponent extends ContainerComponent {
-  constructor (component, data, options) {
-    super(component, data, options);
-  }
-}
-
-export default PanelComponent;
--- forms-flow-web/forms-flow-util/src/plugins/index.js
+++
@@ -1,9 +0,0 @@
-import toHtml from './html';
-import toPdf from './pdf';
-import toXlsx from './xlsx';
-
-export {
-  toHtml,
-  toPdf,
-  toXlsx
-};

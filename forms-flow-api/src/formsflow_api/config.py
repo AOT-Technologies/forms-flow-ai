311b3af5655e1b3413d480d36ef3b5ef91c28833
ratheesh-aot <ratheesh.kumar@aot-technologies.com>
2022-08-11 10:13:52 +0530

    lint issues resolved

--- forms-flow-api/src/formsflow_api/config.py
+++ forms-flow-api/src/formsflow_api/config.py
@@ -91,7 +91,7 @@ class _Config:  # pylint: disable=too-few-public-methods
     FORMIO_USERNAME = os.getenv("FORMIO_ROOT_EMAIL")
     FORMIO_PASSWORD = os.getenv("FORMIO_ROOT_PASSWORD")
     FORMIO_PROJECT_URL = os.getenv("FORMIO_PROJECT_URL")  # For form.io enterprise
-    
+
     CUSTOM_SUBMISSION_URL = os.getenv("CUSTOM_SUBMISSION_URL", "")
     CUSTOM_SUBMISSION_ENABLED = (
         os.getenv("CUSTOM_SUBMISSION_ENABLED", "false").lower() == "true"

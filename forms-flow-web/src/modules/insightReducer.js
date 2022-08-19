5769842e21af039e62fd41fe1d3874c833b7a76b
sreehari ps <sreehari.ps@aot-technologies.com>
2022-08-19 21:36:52 +0530

    insight dashboard updates for dashboard authorization

--- forms-flow-web/src/modules/insightReducer.js
+++ forms-flow-web/src/modules/insightReducer.js
@@ -5,6 +5,10 @@ const initialState = {
   dashboardsList: [],
   dashboardDetail: {},
   isInsightLoading: true,
+  isDashboardListUpdated: false,
+  isDashboardDetailUpdated: false,
+  error: false,
+  errorMessage: null,
 };
 
 const insights = (state = initialState, action) => {
@@ -17,15 +21,19 @@ const insights = (state = initialState, action) => {
         dashboardsList: action.payload,
         isDashboardLoading: false,
         isInsightLoading: false,
+        isDashboardListUpdated: true,
       };
     case ACTION_CONSTANTS.DASHBOARD_DETAIL:
       return {
         ...state,
         dashboardDetail: action.payload,
         isInsightLoading: false,
+        isDashboardDetailUpdated: true,
       };
     case ACTION_CONSTANTS.IS_INSIGHT_DETAIL_LOADING:
       return { ...state, isInsightLoading: action.payload };
+    case ACTION_CONSTANTS.INSIGHT_ERROR:
+      return { ...state, error: true, errorMessage: action.payload };
     default:
       return state;
   }

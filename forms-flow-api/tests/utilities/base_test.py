8c10a903de34c8fa5291f195b628d0c1ea5d65ac
sreehari-aot <93634377+sreehari-aot@users.noreply.github.com>
2022-08-23 21:52:08 +0530

    🐛 Added web form URL to application create request (#784)
    * application create payload update with origin, python start task method updated with origin
    
    * code formatting
    
    * test case update

--- forms-flow-api/tests/utilities/base_test.py
+++ forms-flow-api/tests/utilities/base_test.py
@@ -114,6 +114,7 @@ def get_application_create_payload(form_id: str = "1234"):
         "formId": form_id,
         "submissionId": "1233432",
         "formUrl": f"http://sample.com/form/{form_id}/submission/1233432",
+        "webFormUrl": f"http://sample.com/form/{form_id}/submission/1233432"
     }
 
 

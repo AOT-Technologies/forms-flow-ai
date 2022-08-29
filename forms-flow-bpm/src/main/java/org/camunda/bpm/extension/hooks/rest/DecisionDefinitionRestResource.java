6d63c6fcee7f8104c46318e86f431a000e71ac8a
Sneha Suresh <sneha.suresh@aot-technologies.com>
2022-08-30 02:28:18 +0530

    added dmn APIs (#822)
    Co-authored-by: sneha <sneha@aot-technolo.com>

---
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/rest/DecisionDefinitionRestResource.java
@@ -0,0 +1,33 @@
+package org.camunda.bpm.extension.hooks.rest;
+
+import org.camunda.bpm.engine.rest.dto.repository.DecisionDefinitionDiagramDto;
+import org.camunda.bpm.engine.rest.dto.repository.DecisionDefinitionDto;
+import org.springframework.hateoas.EntityModel;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+@Produces({MediaType.APPLICATION_JSON})
+public interface DecisionDefinitionRestResource extends RestResource {
+
+    String PATH = "/decision-definition";
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    List<DecisionDefinitionDto> getDecisionDefinitions(@Context UriInfo uriInfo,
+                                                       @QueryParam("firstResult") Integer firstResult,
+                                                       @QueryParam("maxResults") Integer maxResults);
+
+    @GET
+    @Path("/key/{key}")
+    @Produces(MediaType.APPLICATION_JSON)
+    EntityModel<DecisionDefinitionDto> getDecisionDefinition(@PathParam("key") String key);
+
+    @GET
+    @Path("/key/{key}/xml")
+    @Produces(MediaType.APPLICATION_JSON)
+    EntityModel<DecisionDefinitionDiagramDto> getDecisionDefinitionDmnXml(@QueryParam("tenantId") String tenantId, @PathParam("key") String key);
+}

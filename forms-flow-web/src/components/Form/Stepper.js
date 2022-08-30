1a431f274488e55795f5da7d9e2dabb062d7a2e9
Abijith Sebastian <98075058+AbijithS-aot@users.noreply.github.com>
2022-08-30 16:19:38 +0530

    Fwf 1670 updated (#828)
    * Fwf 1670
    
    * Update Stepper.js

--- forms-flow-web/src/components/Form/Stepper.js
+++ forms-flow-web/src/components/Form/Stepper.js
@@ -101,7 +101,7 @@ class StepperPage extends PureComponent {
         processList: nextProps.processList,
         processListLoaded: true,
       };
-      nextProps.getAllProcesses();
+      nextProps.getAllProcesses(prevState.tenantKey);
     }
     if (
       nextProps.match.params.formId === FORM_CREATE_ROUTE &&
@@ -415,10 +415,11 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getAllProcesses: () => {
+    getAllProcesses: (tenantKey) => {
+    const tenantIdIn = MULTITENANCY_ENABLED ? tenantKey : null;
       dispatch(
         // eslint-disable-next-line no-unused-vars
-        fetchAllBpmProcesses(true, (err, res) => {
+        fetchAllBpmProcesses(tenantIdIn, (err, res) => {
           if (err) {
             console.log(err);
           }

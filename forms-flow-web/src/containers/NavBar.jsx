628374b0dcf61a0da958e7071b06fa83d9cdc46b
AbijithS-aot <abijith.s@aot-technologies.com>
2022-09-12 15:29:19 +0530

    task menu added

--- forms-flow-web/src/containers/NavBar.jsx
+++ forms-flow-web/src/containers/NavBar.jsx
@@ -8,7 +8,6 @@ import {
   getUserRolePermission,
   // TODO : modify insigth permission conditions
   // getUserInsightsPermission,
-
 } from "../helper/user";
 import createURLPathMatchExp from "../helper/regExp/pathMatch";
 import { useTranslation } from "react-i18next";
@@ -20,7 +19,6 @@ import {
   STAFF_DESIGNER,
   MULTITENANCY_ENABLED,
 } from "../constants/constants";
-import ServiceFlowFilterListDropDown from "../components/ServiceFlow/filter/ServiceTaskFilterListDropDown";
 import { push } from "connected-react-router";
 import i18n from "../resourceBundles/i18n";
 import { setLanguage } from "../actions/languageSetAction";
@@ -137,8 +135,10 @@ const NavBar = React.memo(() => {
                     as={Link}
                     to={`${baseUrl}processes`}
                     className={`main-nav nav-item ${
-                      pathname.match(createURLPathMatchExp("processes", baseUrl)) 
-                        ? "active-tab" 
+                      pathname.match(
+                        createURLPathMatchExp("processes", baseUrl)
+                      )
+                        ? "active-tab"
                         : ""
                     }`}
                   >
@@ -171,48 +171,42 @@ const NavBar = React.memo(() => {
                     </Nav.Link>
                   ) : null
                 ) : null}
-
                 {getUserRolePermission(userRoles, STAFF_REVIEWER) ? (
-                  <NavDropdown
-                    title={
-                      <>
-                        <i className="fa fa-list fa-lg fa-fw mr-2" />
-                        {t("Tasks")}{" "}
-                      </>
-                    }
-                    id="task-dropdown"
+                  <Nav.Link
+                    as={Link}
+                    to={`${baseUrl}task`}
                     className={`main-nav nav-item taskDropdown ${
                       pathname.match(createURLPathMatchExp("task", baseUrl))
-                        ? "active-tab-dropdown"
+                        ? "active-tab"
                         : ""
                     }`}
                     onClick={goToTask}
                   >
-                    <ServiceFlowFilterListDropDown />
-                  </NavDropdown>
+                    {" "}
+                    <i className="fa fa-list fa-lg fa-fw mr-2" />
+                    {t("Tasks")}
+                  </Nav.Link>
                 ) : null}
 
                 {getUserRolePermission(userRoles, STAFF_REVIEWER) ? (
-
-               <Nav.Link
-                      as={Link}
-                      to={`${baseUrl}metrics`}
-                      data-testid="Dashboards"
-                      className={`main-nav nav-item ${
-                        pathname.match(
-                          createURLPathMatchExp("metrics", baseUrl)
-                        ) || pathname.match(
-                              createURLPathMatchExp("insights", baseUrl)
-                            )
-                          ? "active-tab"
-                          : ""
-                      }`}
-                    >
-                      {" "}
-                      <i className="fa fa-tachometer fa-lg fa-fw mr-2" />
-                         {t("Dashboards")}
-                    </Nav.Link>
-                  ) : null}
+                  <Nav.Link
+                    as={Link}
+                    to={`${baseUrl}metrics`}
+                    data-testid="Dashboards"
+                    className={`main-nav nav-item ${
+                      pathname.match(
+                        createURLPathMatchExp("metrics", baseUrl)
+                      ) ||
+                      pathname.match(createURLPathMatchExp("insights", baseUrl))
+                        ? "active-tab"
+                        : ""
+                    }`}
+                  >
+                    {" "}
+                    <i className="fa fa-tachometer fa-lg fa-fw mr-2" />
+                    {t("Dashboards")}
+                  </Nav.Link>
+                ) : null}
               </Nav>
 
               <Nav className="ml-lg-auto mr-auto px-lg-0 px-3">
---
+++ forms-flow-web/src/containers/TaskHead.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ServiceFlowFilterListDropDown from "../components/ServiceFlow/filter/ServiceTaskFilterListDropDown";
+import createURLPathMatchExp from "../helper/regExp/pathMatch";
+import {MULTITENANCY_ENABLED} from "../constants/constants";
+import { NavDropdown } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
+import { push } from 'connected-react-router';
+
+
+function TaskHead() {
+  const dispatch = useDispatch();
+  const tenantKey = useSelector((state) => state.tenants?.tenantId);
+  const selectedFilter = useSelector(state => state.bpmTasks.selectedFilter?.name);
+  const itemCount = useSelector(state => state.bpmTasks.selectedFilter?.itemCount);
+
+  const location = useLocation();
+  const { pathname } = location;
+  const baseUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : "/";
+  const goToTask = () => {
+    dispatch(push(`${baseUrl}task`));
+  };
+
+  return (
+    <div className="header-container">
+    <div className="main-header">
+      
+        <div
+         
+          className='head-item'
+        >
+          <h3 className="application-head">
+            <i
+              className={`fa fa-`}
+              style={{ marginTop: "5px" }}
+              aria-hidden="true"
+            />
+            <span className="application-text">
+                  <NavDropdown
+                  style = {{color:"black"}}
+                  className={`main-nav nav-item taskDropdown  ${
+                    pathname.match(createURLPathMatchExp("task", baseUrl))
+                      ? "active-tab-dropdown"
+                      : ""
+                  }`}
+                    title={
+                      <>
+                        <i className="fa fa-list fa-lg fa-fw " />
+                        {`${selectedFilter ? selectedFilter : 'Tasks'}(${itemCount ? itemCount : ''})`}{" "}
+                      </>
+                    }
+                    onClick={goToTask}
+                  >
+                    <ServiceFlowFilterListDropDown />
+                  </NavDropdown>
+            </span>
+          </h3>
+        </div>
+    </div>
+    <hr className="head-rule" />
+  </div>
+  );
+}
+
+export default TaskHead;

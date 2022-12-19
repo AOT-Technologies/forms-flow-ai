fbd0d76cf48535d3ede0b7b89457a782d650025f
sreehari-aot <93634377+sreehari-aot@users.noreply.github.com>
2022-12-19 09:32:07 +0530

    token verify api added (#1075)

--- forms-flow-api/src/formsflow_api/config.py
+++ forms-flow-api/src/formsflow_api/config.py
@@ -65,6 +65,7 @@ class _Config:  # pylint: disable=too-few-public-methods
     JWT_OIDC_ISSUER = os.getenv("JWT_OIDC_ISSUER")
     JWT_OIDC_AUDIENCE = os.getenv("JWT_OIDC_AUDIENCE")
     JWT_OIDC_CACHING_ENABLED = os.getenv("JWT_OIDC_CACHING_ENABLED")
+    JWT_SECRET = os.getenv('JWT_SECRET')
     JWT_OIDC_JWKS_CACHE_TIMEOUT = 300
 
     # Keycloak Service for BPM Camunda

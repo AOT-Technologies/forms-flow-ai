e51b8f6711330eddc0eff7e5a62b6cdace79941a
Josephalexantony-aot <alex.antony@aot-technologies.com>
2023-01-31 14:06:36 +0530

    File upload modal chages

--- forms-flow-web/src/components/Form/FileUpload/fileUploadModal.js
+++ forms-flow-web/src/components/Form/FileUpload/fileUploadModal.js
@@ -14,12 +14,15 @@ const FileModal = React.memo(({ modalOpen = false, onClose, forms }) => {
   const formUploadCounter = useSelector(
     (state) => state.formCheckList.formUploadCounter
   );
+  const formUploadFailureCounter = useSelector(
+    (state) => state.formCheckList.formUploadFailureCounter
+  );
   const [formsUploaded, setFormsUploaded] = useState(0);
   const { t } = useTranslation();
 
   useEffect(() => {
     if (formUploadList.length) {
-      const uploadloded = (formUploadCounter / formUploadList.length) * 100 ;
+      const uploadloded = (formUploadCounter / formUploadList.length) * 100;
       setFormsUploaded(uploadloded);
       uploadloded === 100 ? toast.success(t("Form Sucessfully uploaded")) : '';
     }
@@ -35,28 +38,32 @@ const FileModal = React.memo(({ modalOpen = false, onClose, forms }) => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>
-            {`${formUploadCounter}/${formUploadList.length} ${
-              formUploadList.length > 1
-                ? t("Forms Completed")
-                : t("Form Completed")
-            }
+          <div style={{ paddingBottom: "7px", color: "#450ccb", fontWeight: "bold" }}>
+            {`${formUploadCounter} of ${formUploadList.length} ${formUploadList.length > 1
+              ? t("forms uploaded")
+              : t("form uploaded")
+              }
             `}
-            {formUploadList.length !== formUploadCounter ? (
+            {formUploadList.length !== formUploadCounter + formUploadFailureCounter ? (
               <Spinner animation="border" variant="primary" />
             ) : (
               ""
             )}
+            {formUploadFailureCounter !== 0 && <span className="fileupload-fail">
+              {
+                formUploadFailureCounter !== 0 ? `Failed to upload ${formUploadFailureCounter} ${formUploadFailureCounter > 1 ? "forms...!" : "form...!"}` : ''
+              }
+            </span>}
           </div>
           {formUploadList.length ? (
-            <div className="progress">
+            <div className="progress" style={{ height: "5px", marginTop: "11px" }}>
               <div
                 className="progress-bar"
                 role="progressbar"
                 aria-valuenow={formsUploaded}
                 aria-label="upload-status"
                 aria-valuemax={`${formsUploaded}`}
-                style={{ width: `${formsUploaded ? "100%" : "0%"}` }}
+                style={{ width: `${formsUploaded === 100 ? "100%" : `${formsUploaded}%`}`, backgroundColor: `${formsUploaded === 100 ? "#00FF00" : ""}` }}
               ></div>
             </div>
           ) : (
@@ -66,7 +73,8 @@ const FileModal = React.memo(({ modalOpen = false, onClose, forms }) => {
           )}
         </Modal.Body>
         <Modal.Footer>
-          <Button type="button" className="btn btn-default" onClick={onClose}>
+
+          <Button style={{ width: "20%" }} type="button" className="btn btn-default" onClick={onClose}>
             <Translation>{(t) => t("Close")}</Translation>
           </Button>
         </Modal.Footer>

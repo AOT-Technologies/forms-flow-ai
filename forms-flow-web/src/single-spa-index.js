2c49cb73c3e0a6030d597b3effdd05f9a9a78128
sreehari ps <sreehari.ps@aot-technologies.com>
2023-02-21 17:26:45 +0530

    Add new module theme
    passing runtime env variables
    fix build issue
    style corrections

--- forms-flow-web/src/single-spa-index.js
+++ forms-flow-web/src/single-spa-index.js
@@ -9,7 +9,6 @@ import { Formio, Components } from "react-formio";
 import { AppConfig } from "./config";
 import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
-import "./styles.scss";
 import "./resourceBundles/i18n.js";
 
 if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === "object") {

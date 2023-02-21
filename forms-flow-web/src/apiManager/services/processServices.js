43ee0758985529b7f10c732d608d6735553a07a5
shuhaib s <shuhaib.s@aot-technologies.com>
2023-02-21 13:14:28 +0530

    removed finally from form status

--- forms-flow-web/src/apiManager/services/processServices.js
+++ forms-flow-web/src/apiManager/services/processServices.js
@@ -22,7 +22,6 @@ import {
   setUnPublishApiError,
   setResetProcess,
   setAllDmnProcessList,
-  setFormStatusLoading,
 } from "../../actions/processActions";
 import { setApplicationCount } from "../../actions/processActions";
 import { replaceUrl } from "../../helper/helper";
@@ -172,8 +171,6 @@ export const getFormProcesses = (formId, ...rest) => {
       .catch((error) => {
         // dispatch(setProcessStatusLoading(false));
         dispatch(setFormProcessLoadError(true));
-      }).finally(()=>{
-        dispatch(setFormStatusLoading(false));
       });
   };
 };

2a0f7a330ce6dc06821ed858c32358dd5917fe39
Josephalexantony-aot <alex.antony@aot-technologies.com>
2023-03-02 13:30:55 +0530

    updated the validator code.

--- forms-flow-web/src/helper/regExp/inputValidator.js
+++ forms-flow-web/src/helper/regExp/inputValidator.js
@@ -1,16 +1,25 @@
-const inputValidator = (searchText) => {
-    let char = '';
-    const a = searchText;
-    if (searchText !== '') {
-        for (let i = 0; i < a?.length; i++) {
-            char = a.charCodeAt(i);
-            if (((char > 32) && (char < 45)) || ((char > 45) && (char < 48))
-                || ((char > 57) && (char < 65)) || ((char > 90) && (char < 95))
-                || ((char > 95) && (char < 97)) || (char > 122)) {
-                return false;
-            }
+const inputValidator = (inputText) => {
+    // Set a flag to indicate whether the input is valid or not
+    let valid = true;
+    // Iterate over each character in the inputText string using a for...of loop
+    for (const char of inputText) {
+        // Get the Unicode code point of the character
+        const charCode = char.charCodeAt(0);
+        console.log(charCode);
+        // Check if the character is not within the valid range of ASCII characters
+        if (((charCode > 32) && (charCode < 45))
+            || ((charCode > 45) && (charCode < 48))
+            || ((charCode > 57) && (charCode < 65))
+            || ((charCode > 90) && (charCode < 95))
+            || ((charCode > 95) && (charCode < 97))
+            || (charCode > 122)) {
+            // If an invalid character is found, set the valid flag to false and exit the loop
+            valid = false;
+            break;
         }
     }
-    return true;
+    // Return the final value of the valid flag
+    return valid;
 };
+// Export the inputValidater function as the default export of the module
 export default inputValidator;

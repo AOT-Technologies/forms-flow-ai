245b9225ad17de2054a8f7de11dff00ea79f6b61
Abil P Raju <83952803+abilpraju-aot@users.noreply.github.com>
2023-03-16 12:51:59 +0530

    user group added

--- forms-flow-web/src/constants/userContants.js
+++ forms-flow-web/src/constants/userContants.js
@@ -1,3 +1,4 @@
 //TODO make it dynamic from env
 /* istanbul ignore file */
-export const REVIEWER_GROUP = "formsflow/formsflow-reviewer";
+import {MULTITENANCY_ENABLED} from "../constants/constants";
+export const REVIEWER_GROUP = MULTITENANCY_ENABLED ? "formsflow-reviewer" : "formsflow/formsflow-reviewer";

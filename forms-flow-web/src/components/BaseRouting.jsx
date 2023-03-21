d1b5935e8002e701e716f9c53f10ef313538c0e7
sreehari ps <sreehari.ps@aot-technologies.com>
2023-03-21 11:26:36 +0530

    bug: routing bugfix

--- forms-flow-web/src/components/BaseRouting.jsx
+++ forms-flow-web/src/components/BaseRouting.jsx
@@ -15,78 +15,86 @@ import i18n from "../resourceBundles/i18n";
 import { updateUserlang } from "../apiManager/services/userservices";
 import { setLanguage } from "../actions/languageSetAction";
 import { initPubSub } from "../actions/pubSubActions";
+import { push } from "connected-react-router";
 
-const BaseRouting = React.memo(({ store, publish, subscribe, getKcInstance }) => {
-  const user = useSelector((state) => state.user);
-  const tenant = useSelector((state) => state.tenants);
-  const dispatch = useDispatch();
-  const isAuth = user.isAuthenticated;
-  const location = useLocation();
+const BaseRouting = React.memo(
+  ({ store, publish, subscribe, getKcInstance }) => {
+    const user = useSelector((state) => state.user);
+    const tenant = useSelector((state) => state.tenants);
+    const dispatch = useDispatch();
+    const isAuth = user.isAuthenticated;
+    const location = useLocation();
+    const [language, setLang] = React.useState(null);
 
-  const [language, setLang] = React.useState(null);
+    React.useEffect(() => {
+      if (window.location.pathname !== location.pathname) {
+        dispatch(push(window.location.pathname));
+      }
+    }, []);
 
-  React.useEffect(()=>{
-    dispatch(initPubSub({publish, subscribe}));  
-  },[publish, subscribe]);
+    React.useEffect(() => {
+      dispatch(initPubSub({ publish, subscribe }));
+    }, [publish, subscribe]);
 
-  subscribe("ES_CHANGE_LANGUAGE", (msg, data) => {
-    i18n.changeLanguage(data);
-  });
+    subscribe("ES_CHANGE_LANGUAGE", (msg, data) => {
+      i18n.changeLanguage(data);
+    });
 
-  subscribe("ES_UPDATE_LANGUAGE", (msg, data) => {
-    setLang(data);
-  });
+    subscribe("ES_UPDATE_LANGUAGE", (msg, data) => {
+      setLang(data);
+    });
 
-  React.useEffect(() => {
-    if (user) {
-      publish("ES_USER", user);
-    }
-    if (tenant) {
-      publish("ES_TENANT", tenant);
-    }
-  }, [user, tenant]);
+    React.useEffect(() => {
+      if (user) {
+        publish("ES_USER", user);
+      }
+      if (tenant) {
+        publish("ES_TENANT", tenant);
+      }
+    }, [user, tenant]);
 
-  React.useEffect(() => {
-    publish("ES_ROUTE", location);
-  }, [location]);
+    React.useEffect(() => {
+      publish("ES_ROUTE", location);
+    }, [location]);
 
-  React.useEffect(() => {
-    if (language) {
-      dispatch(setLanguage(language));
-      dispatch(updateUserlang(language));
-    }
-  }, [language]);
+    React.useEffect(() => {
+      if (language) {
+        dispatch(setLanguage(language));
+        dispatch(updateUserlang(language));
+      }
+    }, [language]);
 
-  return (
-    <>
-      <div className="wrapper">
-        <div className="container-fluid content main-container">
-          <ToastContainer />
-          <Switch>
-            <Route path="/public">
-              <PublicRoute
-                store={store}
-                publish={publish}
-                subscribe={subscribe}
-                getKcInstance={getKcInstance}
-              />
-            </Route>
-            <Route path={BASE_ROUTE}>
-              <PrivateRoute
-                store={store}
-                publish={publish}
-                subscribe={subscribe}
-                getKcInstance={getKcInstance}
-              />
-            </Route>
-            <Route path="/404" exact={true} component={NotFound} />
-            <Redirect from="*" to="/404" />
-          </Switch>
-          {isAuth ? <Footer /> : null}
+    return (
+      <>
+        <div className="wrapper">
+          <div className="container-fluid content main-container">
+            <ToastContainer />
+            <Switch>
+              <Route path="/public">
+                <PublicRoute
+                  store={store}
+                  publish={publish}
+                  subscribe={subscribe}
+                  getKcInstance={getKcInstance}
+                />
+              </Route>
+              <Route path={BASE_ROUTE}>
+                <PrivateRoute
+                  store={store}
+                  publish={publish}
+                  subscribe={subscribe}
+                  getKcInstance={getKcInstance}
+                />
+              </Route>
+              <Route path="/404" exact={true} component={NotFound} />
+              <Redirect from="*" to="/404" />
+            </Switch>
+            {isAuth ? <Footer /> : null}
+          </div>
         </div>
-      </div>
-    </>
-  );
-});
+      </>
+    );
+  }
+);
 
 export default BaseRouting;

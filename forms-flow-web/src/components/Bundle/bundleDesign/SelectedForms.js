1e5a6c8c79035126ff0bd17a7686fd546041e85f
AbijithS-aot <abijith.s@aot-technologies.com>
2023-03-23 14:51:36 +0530

    draggable table added for bundle forms table inside create bundle

--- forms-flow-web/src/components/Bundle/bundleDesign/SelectedForms.js
+++ forms-flow-web/src/components/Bundle/bundleDesign/SelectedForms.js
@@ -1,14 +1,15 @@
-import React from "react";
-import Table from "@material-ui/core/Table";
+import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import { withStyles } from "@material-ui/styles";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { MULTITENANCY_ENABLED } from "../../../constants/constants";
 import _capitalize from "lodash/capitalize";
+import { setBundleSelectedForms } from "../../../actions/bundleActions";
 const StyledTableCell = withStyles(() => ({
   head: {
     backgroundColor: "#4559b5",
@@ -21,37 +22,71 @@ const StyledTableCell = withStyles(() => ({
 }))(TableCell);
 
 const SelectedForms = ({ handleModalChange, selectedForms, deleteForm }) => {
+  console.log("selected forms",selectedForms);
+  const dispatch = useDispatch();
+  const [items, setItems] = useState(selectedForms);
+  console.log("items",items);
   const tenantKey = useSelector((state) => state.tenants?.tenantId);
   const redirectUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : "/";
+
+  useEffect(()=>{
+    setItems(selectedForms);
+  },[selectedForms]);
 
   const viewForm = (formId) => {
     window.open(`${redirectUrl}formflow/${formId}/preview`, "_blank");
   };
+  const onDragStart = (e, index) => {
+    e.dataTransfer.setData("index", index);
+    };
+    
+    const onDragOver = (e) => {
+    e.preventDefault();
+    };
 
+    
+    const onDrop = (e, index) => {
+    const oldIndex = e.dataTransfer.getData("index");
+    const newArray = [...items];
+    const draggedItem = newArray[oldIndex];
+    newArray.splice(oldIndex, 1);
+    newArray.splice(index, 0, draggedItem);
+    newArray.forEach((item, i) => {
+      item.formOrder = i + 1;
+    });
+    setItems(newArray);
+    dispatch(setBundleSelectedForms(newArray));
+    };
   return (
     <div>
-      <TableContainer style={{ border: "1px solid #dbdbdb" }}>
+      <TableContainer style={{ border: "1px solid #dbdbdb" }} striped bordered hover>
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <StyledTableCell align="left">Form Order</StyledTableCell> 
+              <StyledTableCell align="left">Form Order</StyledTableCell>
               <StyledTableCell align="left">Form Name</StyledTableCell>
               <StyledTableCell align="left">Form Type</StyledTableCell>
               <StyledTableCell align="right">View</StyledTableCell>
               <StyledTableCell align="right">Action</StyledTableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
-            {selectedForms?.map((form) => (
-              <TableRow key={form.id}>
-                <StyledTableCell>{form.formOrder}</StyledTableCell> 
-                <StyledTableCell>{form.formName}</StyledTableCell>
-                <StyledTableCell>{_capitalize(form.formType)}</StyledTableCell>
+          <TableBody style={{cursor:'move'}}>
+            {items?.map((item, index) => (
+              <TableRow
+                key={item.id}
+                draggable
+                onDragStart={(e) => onDragStart(e, index)}
+                onDragOver={onDragOver}
+                onDrop={(e) => onDrop(e, index)}
+              >
+                <StyledTableCell>{index + 1}</StyledTableCell>
+                <StyledTableCell>{item.formName}</StyledTableCell>
+                <StyledTableCell>{_capitalize(item.formType)}</StyledTableCell>
                 <StyledTableCell align="right">
                   <button
                     className="btn btn-sm btn-outline-primary"
                     onClick={() => {
-                      viewForm(form.formId);
+                      viewForm(item.formId);
                     }}
                   >
                     <i
@@ -65,7 +100,7 @@ const SelectedForms = ({ handleModalChange, selectedForms, deleteForm }) => {
                   <button
                     className="btn btn-sm btn-outline-danger"
                     onClick={() => {
-                      deleteForm(form.parentFormId);
+                      deleteForm(item.parentFormId);
                     }}
                   >
                     <i className="fa fa-trash-o" aria-hidden="true"></i>

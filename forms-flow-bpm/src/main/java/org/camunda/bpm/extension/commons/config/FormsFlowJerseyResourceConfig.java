b4a33ef509f40fe5d8bf04a34193488af9a896c6
sneha <sneha@aot-technolo.com>
2023-05-16 16:37:33 +0530

    Update FormsFlowJerseyResourceConfig.java
    changes to avoid conflicts with FormsFlowProcessEngineRestServiceImpl extended resourses

--- forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/config/FormsFlowJerseyResourceConfig.java
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/config/FormsFlowJerseyResourceConfig.java
@@ -2,6 +2,7 @@ package org.camunda.bpm.extension.commons.config;
 
 
 import org.camunda.bpm.engine.rest.impl.CamundaRestResources;
+import org.camunda.bpm.engine.rest.impl.DefaultProcessEngineRestServiceImpl;
 import org.camunda.bpm.extension.hooks.rest.impl.FormsFlowProcessEngineRestServiceImpl;
 import org.camunda.bpm.spring.boot.starter.rest.CamundaJerseyResourceConfig;
 import org.glassfish.jersey.jackson.JacksonFeature;
@@ -18,6 +19,8 @@ public class FormsFlowJerseyResourceConfig extends CamundaJerseyResourceConfig {
 
 
     protected void registerCamundaRestResources() {
+        //Removing so that it won't conflict with the FormsFlowProcessEngineRestServiceImpl extended resources
+        CamundaRestResources.getResourceClasses().remove(DefaultProcessEngineRestServiceImpl.class);
         this.registerClasses(CamundaRestResources.getResourceClasses());
         this.registerClasses(CamundaRestResources.getConfigurationClasses());
         this.register(JacksonFeature.class);

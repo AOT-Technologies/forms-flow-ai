66b831a45324172240592d619df17a852ea27f55
Don Basil Peter <don.peter@aot-technologies.com>
2023-06-11 21:39:18 -0700

    updates in task list layout

--- forms-flow-web/src/components/ServiceFlow/list/ServiceTaskListView.js
+++ forms-flow-web/src/components/ServiceFlow/list/ServiceTaskListView.js
@@ -20,6 +20,7 @@ import { MAX_RESULTS } from "../constants/taskConstants";
 import { getFirstResultIndex } from "../../../apiManager/services/taskSearchParamsFormatterService";
 import TaskVariable from "./TaskVariable";
 import { MULTITENANCY_ENABLED } from "../../../constants/constants";
+import TaskHeaderListView from "../details/TaskHeaderListView";
 const ServiceTaskListView = React.memo(() => {
   const { t } = useTranslation();
   const taskList = useSelector((state) => state.bpmTasks.tasksList);
@@ -74,16 +75,17 @@ const ServiceTaskListView = React.memo(() => {
               }`}
               key={index}
             >
-              <Row className="p-6">
-                <Col md={4}>
+              <div className="d-flex mt-4 justify-content-between">
+
+                <div>
                   <div className="col-12">
                     <h4 className="font-weight-bold">{task.name}</h4>
                   </div>
                   <div className="col-12" style={{ paddingTop: "1rem" }}>
                     <h6 className="font-weight-light">{task.id}</h6>
                   </div>
-                </Col>
-                <Col md={1}>
+                </div>
+                <div>
                   <div className="col-12">
                     <h6 className="font-weight-light">Priority</h6>
                   </div>
@@ -102,99 +104,17 @@ const ServiceTaskListView = React.memo(() => {
                       <u className="font-weight-bold p-2">{task.priority}</u>
                     </h6>
                   </div>
-                </Col>
-                <Col md={1}>
-                  <div className="col-12">
-                    <h6 className="font-weight-light">Follow Date</h6>
-                  </div>
-                  <div className="d-flex col-12">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      class="bi bi-calendar"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-                    </svg>
-                    <h6>
-                      <u className="font-weight-bold p-2">Add Date</u>
-                    </h6>
-                  </div>
-                </Col>
-                <Col md={1}>
-                  <div className="col-12">
-                    <h6 className="font-weight-light">Due Date</h6>
-                  </div>
-                  <div className="d-flex col-12">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      class="bi bi-calendar"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-                    </svg>
-                    <h6>
-                      <u className="font-weight-bold p-2">Add Date</u>
-                    </h6>
-                  </div>
-                </Col>
-                <Col md={1}>
-                  <div className="col-12">
-                    <h6 className="font-weight-light">Groups</h6>
-                  </div>
-                  <div className="d-flex col-12">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      class="bi bi-people"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
-                    </svg>
-                    <h6>
-                      <u className="font-weight-bold p-2">Add Group</u>
-                    </h6>
-                  </div>
-                </Col>
-                <Col md={1}>
-                  <div className="col-12">
-                    <h6 className="font-weight-light">Assigne</h6>
-                  </div>
-                  <div className="d-flex col-12">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      class="bi bi-person-circle"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                      <path
-                        fill-rule="evenodd"
-                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                      />
-                    </svg>
-                    <h6>
-                      <u className="font-weight-bold p-2">{task.assignee}</u>
-                    </h6>
-                  </div>
-                </Col>
-                <Col md={2}>
-                  <div className="col-12" style={{ paddingTop: "1rem" }}>
+                </div>
+                <TaskHeaderListView/>
+                 <div className="flex-fill"></div>
+                <div className="pr-4">
+                  <div className="col-12 mt-3">
                     <h6>
                       <u className="font-weight-light">View Details</u>
                     </h6>
                   </div>
-                </Col>
-              </Row>
+                </div>
+              </div>
             </div>
           ))}
 
@@ -226,7 +146,7 @@ const ServiceTaskListView = React.memo(() => {
   return (
     <>
       <ListGroup className="service-task-list">
-        <TaskFilterComponent totalTasks={isTaskListLoading ? 0 : tasksCount} />
+        {/* <TaskFilterComponent totalTasks={isTaskListLoading ? 0 : tasksCount} /> */}
         {isTaskListLoading ? <Loading /> : renderTaskList()}
       </ListGroup>
     </>

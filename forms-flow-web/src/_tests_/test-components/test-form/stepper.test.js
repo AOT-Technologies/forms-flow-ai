773d72ec7bb61c0059d69025c2db14ab9055de63
abhilash-aot <abhilash.kr@aot-technologies.com>
2023-06-22 05:15:05 -0700

    updated test

--- forms-flow-web/src/_tests_/test-components/test-form/stepper.test.js
+++ forms-flow-web/src/_tests_/test-components/test-form/stepper.test.js
@@ -1,10 +1,10 @@
-//import React from "react";
-//import { render as rtlRender } from "@testing-library/react";
-//import Stepper from "../../../components/Form/Stepper";
+import React from "react";
+import {render as rtlRender} from "@testing-library/react";
+import Stepper from "../../../components/Form/Stepper";
 import "@testing-library/jest-dom/extend-expect";
-//import { Provider } from "react-redux";
-//import { Router, Route } from "react-router";
-//import { createMemoryHistory } from "history";
+import { Provider } from "react-redux";
+import { Router, Route } from "react-router";
+import { createMemoryHistory } from "history";
 import configureStore from "redux-mock-store";
 import { mockstate } from "./constants";
 
@@ -17,7 +17,7 @@ beforeEach(() => {
   store.dispatch = jest.fn();
 });
 
-/*function renderWithRouterMatch(
+function renderWithRouterMatch(
   Ui,
   {
     path = "/",
@@ -34,32 +34,30 @@ beforeEach(() => {
       </Provider>
     ),
   };
-}*/
+}
 
 beforeEach(() => {
   store = mockStore(mockstate);
   store.dispatch = jest.fn();
 });
 
-/*
+
 describe('Stepper', () => {
-
-it('does not render any steps when formProcessListCount is not 1', () => {
-      const { queryByText } = renderWithRouterMatch(Stepper, {
-        path: "/formflow/:formId?/:step?",
-        route: "/formflow/create",
-      });
-      const componentInstance = queryByText('Design Form');
-
-      expect(componentInstance).toBeNull();
-
-      const associateForm = queryByText('Associate this form with a workflow?');
-
-      expect(associateForm).toBeNull();
-
-      const previewConfirm = queryByText('Preview and Confirm');
-
-      expect(previewConfirm).toBeNull();
+  it("should render the stepper component without break",()=>{
+    const { queryByText } = renderWithRouterMatch(Stepper, {
+      path: "/formflow/:formId?/:step?",
+      route: "/formflow/create",
     });
+    const componentInstance = queryByText('Design Form');
 
-}); */
+    expect(componentInstance).toBeInTheDocument();
+
+    const associateForm = queryByText('Associate this form with a workflow?');
+
+    expect(associateForm).toBeInTheDocument();
+
+    const previewConfirm = queryByText('Preview and Confirm');
+
+    expect(previewConfirm).toBeInTheDocument();
+  });
+});

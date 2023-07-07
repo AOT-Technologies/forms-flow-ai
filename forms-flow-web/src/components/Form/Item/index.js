d37896aabc23e912f6f99360e93ffb75b4d583f0
shuhaib-aot <shuhaib.s@aot-technologies.com>
2023-07-07 13:03:20 +0530

    fixed auth checking

--- forms-flow-web/src/components/Form/Item/index.js
+++ forms-flow-web/src/components/Form/Item/index.js
@@ -5,14 +5,11 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   STAFF_REVIEWER,
   CLIENT,
-  STAFF_DESIGNER,
   BASE_ROUTE,
   MULTITENANCY_ENABLED,
 } from "../../../constants/constants";
 import View from "./View";
-import Edit from "./Edit";
 import Submission from "./Submission/index";
-import Preview from "./Preview";
 import { checkIsObjectId } from "../../../apiManager/services/formatterService";
 import { fetchFormByAlias } from "../../../apiManager/services/bpmFormServices";
 import {
@@ -58,10 +55,9 @@ const Item = React.memo(() => {
       dispatch(getForm("form", formId,(err,res)=>{
         if(err){
           dispatch(setFormAuthVerifyLoading(false));
-        }else{
-           if(!userRoles.includes(STAFF_REVIEWER) && 
-           (userRoles.includes(CLIENT) || userRoles.includes(STAFF_DESIGNER))){
-          formAuthVerify(res.parentFormId || res._id);
+        }else{          
+          if(!userRoles.includes(STAFF_REVIEWER) && userRoles.includes(CLIENT)){
+            formAuthVerify(res.parentFormId || res._id);
           }else{
             dispatch(setFormAuthVerifyLoading(false));
           }
@@ -120,34 +116,15 @@ const Item = React.memo(() => {
       }
     />
   );
+
   /**
    * Protected route for form deletion
    */
-  const FormActionRoute = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={(props) =>
-        userRoles.includes(STAFF_DESIGNER) ? (
-          <Component {...props} />
-        ) : (
-          <Redirect exact to={`${redirectUrl}`} />
-        )
-      }
-    />
-  );
 
   return (
     <div>
       <Switch>
         <Route exact path={`${BASE_ROUTE}form/:formId`} component={View} />
-        <FormActionRoute
-          path={`${BASE_ROUTE}form/:formId/preview`}
-          component={Preview}
-        />
-        <FormActionRoute
-          path={`${BASE_ROUTE}form/:formId/edit`}
-          component={Edit}
-        />
         <SubmissionRoute
           path={`${BASE_ROUTE}form/:formId/submission`}
           component={Submission}

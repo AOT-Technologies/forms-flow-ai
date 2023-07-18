7946a6379769ef12f1c98421ac4b90816f6b182f
shuhaib-aot <shuhaib.s@aot-technologies.com>
2023-07-18 14:44:35 +0530

    added fallback

--- forms-flow-web/src/resourceBundles/i18n.js
+++ forms-flow-web/src/resourceBundles/i18n.js
@@ -1,9 +1,8 @@
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import {i18nService} from "@formsflow/service";
-i18nService.use(LanguageDetector)
-.use(initReactI18next)
-.init();
-
+import { i18nService } from "@formsflow/service";
+i18nService.use(LanguageDetector).use(initReactI18next).init({
+  fallbackLng: "en",
+});
 
 export default i18nService;

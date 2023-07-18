47f55c1fa36829f5de5fb116e2a176fb733ed545
shuhaib-aot <shuhaib.s@aot-technologies.com>
2023-07-18 15:20:45 +0530

    test case fixed

--- forms-flow-web/src/resourceBundles/i18n.js
+++ forms-flow-web/src/resourceBundles/i18n.js
@@ -1,7 +1,7 @@
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { i18nService } from "@formsflow/service";
-i18nService.use(LanguageDetector).use(initReactI18next).init({
+i18nService?.use(LanguageDetector).use(initReactI18next).init({
   fallbackLng: "en",
 });
 

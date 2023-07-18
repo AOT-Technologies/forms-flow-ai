f174aa77585d30be696cee6a84c6e41576849638
shuhaib-aot <shuhaib.s@aot-technologies.com>
2023-07-18 13:18:37 +0530

    updated i18n

--- forms-flow-web/src/resourceBundles/i18n.js
+++ forms-flow-web/src/resourceBundles/i18n.js
@@ -1,7 +1,6 @@
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import {i18nService} from "@formsflow/service";
-
 i18nService.use(LanguageDetector)
 .use(initReactI18next)
 .init();

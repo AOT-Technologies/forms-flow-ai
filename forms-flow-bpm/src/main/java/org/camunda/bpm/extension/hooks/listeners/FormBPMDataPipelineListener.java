860397d0bba7415adb886d512dcf7ca0b01b2089
SnehaSuresh-aot <sneha.suresh@aot-technologies.com>
2023-09-15 08:11:14 +0530

    moved java DI  to spring DI

--- forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/FormBPMDataPipelineListener.java
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/FormBPMDataPipelineListener.java
@@ -9,8 +9,9 @@ import org.camunda.bpm.extension.hooks.services.FormSubmissionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-import javax.inject.Named;
 import java.io.IOException;
 import java.util.Map;
 
@@ -19,7 +20,9 @@ import static org.camunda.bpm.extension.commons.utils.VariableConstants.FORM_URL
  * FormBPMDataPipelineListener.
  * This class copies all the form document data into CAM variables.
  */
-@Named("FormBPMDataPipelineListener")
+
+@Qualifier("FormBPMDataPipelineListener")
+@Component
 public class FormBPMDataPipelineListener  extends BaseListener implements TaskListener, ExecutionListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FormBPMDataPipelineListener.class);

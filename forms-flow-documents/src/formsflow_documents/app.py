3b9af0806f5910fb14d8174e8e77825af070cae6
auslin-aot <auslin.pajila@aot-technologies.com>
2023-10-26 14:57:16 +0530

    Added  file logs to Documents API

--- forms-flow-documents/src/formsflow_documents/app.py
+++ forms-flow-documents/src/formsflow_documents/app.py
@@ -14,9 +14,9 @@ from formsflow_api_utils.utils import (
     ALLOW_ALL_ORIGINS,
     CORS_ORIGINS,
     FORMSFLOW_API_CORS_ORIGINS,
-    CustomFormatter,
     cache,
     jwt,
+    register_log_handlers,
     setup_logging,
     translate,
 )
@@ -47,10 +47,13 @@ def create_app(run_mode=os.getenv("FLASK_ENV", "production")):
     )
     app.logger = flask_logger
     app.logger = logging.getLogger("app")
-    logs = logging.StreamHandler()
-
-    logs.setFormatter(CustomFormatter())
-    app.logger.handlers = [logs]
+    register_log_handlers(
+        app,
+        log_file="logs/forms-flow-documents-api.log",
+        when=os.getenv("API_LOG_ROTATION_WHEN", "d"),
+        interval=int(os.getenv("API_LOG_ROTATION_INTERVAL", "1")),
+        backupCount=int(os.getenv("API_LOG_BACKUP_COUNT", "7")),
+    )
     app.logger.propagate = False
     logging.log.propagate = False
     with open("logo.txt") as file:  # pylint: disable=unspecified-encoding

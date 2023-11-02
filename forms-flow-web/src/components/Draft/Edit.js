a30a13e71e7aac76faadf279e3d1a5c37d113482
shuhaib-aot <shuhaib.s@aot-technologies.com>
2023-11-02 10:10:06 -0700

    Fixed fetchAllprocess list function breaking and give proper title to back button

--- forms-flow-web/src/components/Draft/Edit.js
+++ forms-flow-web/src/components/Draft/Edit.js
@@ -244,7 +244,7 @@ const View = React.memo((props) => {
             onConfirm={props.onConfirm}
           ></SubmissionError>
           {isAuthenticated ? (
-            <Link title={t("go back")} to={`${redirectUrl}draft`}>
+            <Link title={t("Back to Drafts")} to={`${redirectUrl}draft`}>
               <i className="fa fa-chevron-left fa-lg" />
             </Link>
           ) : null}

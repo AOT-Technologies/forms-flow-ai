9946ba19e5c76f0e41a27a8ffa3baf3af0647706
Bonymol-aot <bonymol@aot-technologies.com>
2023-11-09 23:39:12 -0800

    UI update

--- forms-flow-web/src/components/Form/constants/ClientTable.js
+++ forms-flow-web/src/components/Form/constants/ClientTable.js
@@ -1,15 +1,17 @@
 /* eslint-disable */
 import React, { useState, useEffect } from "react";
-import { InputGroup, FormControl, Dropdown } from "react-bootstrap";
+import { InputGroup, FormControl, Dropdown, Collapse } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
 import Pagination from "react-js-pagination";
 import { setBPMFormLimit, setBPMFormListPage, setBPMFormListSort, setBpmFormSearch, setFormDeleteStatus } from "../../../actions/formActions";
-import SelectFormForDownload from "../FileUpload/SelectFormForDownload";
 import LoadingOverlay from "react-loading-overlay";
 import { MULTITENANCY_ENABLED, STAFF_DESIGNER } from "../../../constants/constants";
 import { useTranslation } from "react-i18next";
 import { Translation } from "react-i18next";
+import {sanitize} from "dompurify";
+
+
 
 
 function ClientTable() {
@@ -30,21 +32,13 @@ function ClientTable() {
   const searchText = useSelector((state) => state.bpmForms.searchText);
   const [search, setSearch] = useState(searchText || "");
   const redirectUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : "/";
+  const [openIndex, setOpenIndex] = useState(-1);
 
 
-
-
-  const [openIndex, setOpenIndex] = useState(-1); 
-
-  const toggleDropdown = (index) => {
-    if (openIndex === index) {
-      
-      setOpenIndex(-1);
-    } else {
-      
-      setOpenIndex(index);
-    }
+  const handleToggle = (index) => {
+    setOpenIndex(openIndex === index ? -1 : index);
   };
+
 
   const pageOptions = [
     { text: "5", value: 5 },
@@ -54,15 +48,17 @@ function ClientTable() {
     { text: "All", value: totalForms },
   ];
 
+
   const updateSort = (updatedSort) => {
     dispatch(setBPMFormListSort(updatedSort));
     dispatch(setBPMFormListPage(1));
   };
 
-  
+
   useEffect(() => {
     setSearch(searchText);
   }, [searchText]);
+
 
   useEffect(() => {
     if (!search?.trim()) {
@@ -70,23 +66,28 @@ function ClientTable() {
     }
   }, [search]);
 
+
   const handleSearch = () => {
     dispatch(setBpmFormSearch(search));
     dispatch(setBPMFormListPage(1));
   };
 
+
   const submitNewForm = (formId) => {
     dispatch(push(`${redirectUrl}form/${formId}`));
   };
+
 
   const handleClearSearch = () => {
     setSearch("");
     dispatch(setBpmFormSearch(""));
   };
 
+
   const handlePageChange = (page) => {
     dispatch(setBPMFormListPage(page));
   };
+
 
   const onSizePerPageChange = (limit) => {
     setPageLimit(limit);
@@ -104,19 +105,6 @@ function ClientTable() {
   );
 
 
-  const [selectedRow, setselectedRow] = useState([]);
-
-  const handleRowExpansion = (index) => {
-    const currentIndex = selectedRow.indexOf(index);
-    const newselectedRow = [...selectedRow];
-    if (currentIndex === -1) {
-      newselectedRow.push(index);
-    } else {
-      newselectedRow.splice(currentIndex, 1);
-    }
-    setselectedRow(newselectedRow);
-  };
-
   const noDataFound = () => {
     return (
       <tbody>
@@ -132,6 +120,10 @@ function ClientTable() {
     );
   };
 
+
+  const sampleData = "<p>This is a sample data frmom html. <b>Bold</b> or <i>Italic</i> text.</p><br>Normal text for the sample data.";
+
+
   return (
     <>
       <LoadingOverlay active={searchFormLoading} spinner text="Loading...">
@@ -141,7 +133,7 @@ function ClientTable() {
               <tr>
                 <th>
                   <div className="d-flex align-items-center">
-                    {isDesigner && <SelectFormForDownload type="all" />}
+                    {/* {isDesigner && <SelectFormForDownload type="all" />} */}
                     <span className="ml-4 mt-1">{t("Form Title")}</span>
                     <span>
                       {isAscending ? (
@@ -206,48 +198,48 @@ function ClientTable() {
                 </th>
               </tr>
             </thead>
-
             {formData?.length ? (
-        <tbody>
-          {formData?.map((e, index) => {
-            return (
-              <React.Fragment key={index}>
-                <tr>
-                  <td className="col-4">
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      {!isDesigner && (
-                        <i
-                          className={`fa fa-chevron-${
-                            selectedRow.includes(index) ? "up" : "down"
-                          }`}
-                          style={{ cursor: 'pointer' }}
-                          onClick={() => handleRowExpansion(index)}
-                        ></i>
-                      )}
-                      <span className="ml-2 mt-2">{e.title}</span>
-                    </div>
-                  </td>
-                  <td>{e.description}</td>
-                  {!isDesigner && <td>{submitNew(e)}</td>}
-                  <td style={{ position: "relative" }}></td>
-                </tr>
-                {selectedRow.includes(index) && (
-                  <tr>
-                    <td colSpan="4">{e.description}</td>
-                  </tr>
+  <tbody>
+    {formData.map((e, index) => (
+      <React.Fragment key={index}>
+        <tr>
+          <td className="col-4">
+            <div>
+              <div className="d-flex align-items-center">
+                {!isDesigner && (
+                  <i
+                    className={`fa ${openIndex === index ? "fa-chevron-up" : "fa-chevron-down"}`}
+                    onClick={() => handleToggle(index)}
+                  ></i>
                 )}
-              </React.Fragment>
-            );
-          })}
-        </tbody>
-      ) : !searchFormLoading ? (
-        noDataFound()
-      ) : (
-        ""
-      )}
+                <span className="ml-2 mt-2">{e.title}</span>
+              </div>
+              <div className="">
+              <Collapse in={openIndex === index}>
+  <div
+    className="mt-2"
+    dangerouslySetInnerHTML={{ __html: sampleData }}
+  />
+</Collapse>
+              </div>
+            </div>
+          </td>
+          <td>{e.description}</td>
+          {!isDesigner && <td>{submitNew(e)}</td>}
+          <td style={{ position: "relative" }}></td>
+        </tr>
+      </React.Fragment>
+    ))}
+  </tbody>
+) : !searchFormLoading ? (
+  noDataFound()
+) : (
+  ""
+)}
           </table>
         </div>
       </LoadingOverlay>
+
 
       {formData.length ? (
         <div className="d-flex justify-content-between align-items-center flex-column flex-md-row">
@@ -257,6 +249,7 @@ function ClientTable() {
               <Dropdown.Toggle variant="light" id="dropdown-basic">
                 {pageLimit}
               </Dropdown.Toggle>
+
 
               <Dropdown.Menu>
                 {pageOptions.map((option, index) => (
@@ -296,5 +289,6 @@ function ClientTable() {
     </>
   );
 }
+
 
 export default ClientTable;

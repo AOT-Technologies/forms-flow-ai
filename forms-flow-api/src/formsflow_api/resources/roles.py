a99a48d1659d67984eda01307b3b04a5f3b4e2d8
auslin-aot <99173163+auslin-aot@users.noreply.github.com>
2023-11-16 11:53:03 +0530

    Make roles endpoint accessible to reviewers. (#1726)

--- forms-flow-api/src/formsflow_api/resources/roles.py
+++ forms-flow-api/src/formsflow_api/resources/roles.py
@@ -6,6 +6,7 @@ from flask_restx import Namespace, Resource, fields
 from formsflow_api_utils.utils import (
     ADMIN_GROUP,
     DESIGNER_GROUP,
+    REVIEWER_GROUP,
     auth,
     cors_preflight,
     profiletime,
@@ -35,7 +36,7 @@ class KeycloakRolesResource(Resource):
     """Resource to manage keycloak list and create roles/groups."""
 
     @staticmethod
-    @auth.has_one_of_roles([ADMIN_GROUP, DESIGNER_GROUP])
+    @auth.has_one_of_roles([ADMIN_GROUP, DESIGNER_GROUP, REVIEWER_GROUP])
     @profiletime
     @API.doc(
         responses={

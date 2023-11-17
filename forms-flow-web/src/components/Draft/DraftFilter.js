e37e6264c62291b00893d35990f90e7288f4699b
Abijith Sebastian <98075058+AbijithS-aot@users.noreply.github.com>
2023-11-17 15:49:20 +0530

    Sort fixes (#1736)
    * changed labels for current user task
    
    * sort for application and draft
    
    * overlay loading added
    
    * labels in filters changes
    
    * added margin and arrow dorection changes for sort icons
    
    ---------
    
    Co-authored-by: AbijithS-aot <abijith.s@aot-technololgies.com>

--- forms-flow-web/src/components/Draft/DraftFilter.js
+++ forms-flow-web/src/components/Draft/DraftFilter.js
@@ -4,6 +4,7 @@ import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDrafts } from "../../apiManager/services/draftService";
 import { useTranslation } from "react-i18next";
+import { setDraftListLoading } from "../../actions/draftActions";
 
 const DraftFilter = ({ setDisplayFilter, filterParams, setFilterParams }) => {
   const dispatch = useDispatch();
@@ -15,6 +16,8 @@ const DraftFilter = ({ setDisplayFilter, filterParams, setFilterParams }) => {
   );
   const pageNo = useSelector((state) => state.draft?.activePage);
   const limit = useSelector((state) => state.draft?.countPerPage);
+  const sortOrder = useSelector((state) => state.draft.sortOrder);
+  const sortBy = useSelector((state) => state.draft.sortBy);
   const { t } = useTranslation();
   const handleClick = (e) => {
     if (createSearchNode?.current?.contains(e.target)) {
@@ -38,6 +41,7 @@ const DraftFilter = ({ setDisplayFilter, filterParams, setFilterParams }) => {
   };
 
   const clearAllFilters = () => {
+    dispatch(setDraftListLoading(true));
     setDraftId("");
     setDraftName("");
     setLastModified(null);
@@ -48,20 +52,33 @@ const DraftFilter = ({ setDisplayFilter, filterParams, setFilterParams }) => {
       modified: null,
       page: pageNo,
       limit: limit,
+      sortOrder,
+      sortBy
     };
-    dispatch(fetchDrafts(filters));
+    dispatch(fetchDrafts(filters,(err,data) => {
+      if(data){
+      dispatch(setDraftListLoading(false));
+      }
+    }));
   };
 
   const applyFilters = () => {
+    dispatch(setDraftListLoading(true));
     let filterParams = {
       draftName: draftName,
       id: draftId,
       modified: lastModified,
       page: pageNo,
       limit: limit,
+      sortOrder,
+      sortBy
     };
     setFilterParams(filterParams);
-    dispatch(fetchDrafts(filterParams));
+    dispatch(fetchDrafts(filterParams,(err,data) => {
+      if(data){
+      dispatch(setDraftListLoading(false));
+      }
+    }));
   };
 
   return (
@@ -85,7 +102,7 @@ const DraftFilter = ({ setDisplayFilter, filterParams, setFilterParams }) => {
       <div className="m-4 px-2">
         <Row className="mt-2">
           <Col>
-          <label>{t("Draft Id")}</label>
+          <label>{t("Id")}</label>
             <input
               className="form-control"
               placeholder=""
@@ -94,7 +111,7 @@ const DraftFilter = ({ setDisplayFilter, filterParams, setFilterParams }) => {
             />
           </Col>
           <Col>
-          <label>{t("Draft Name")}</label>
+          <label>{t("Title")}</label>
             <input
               className="form-control"
               placeholder=""

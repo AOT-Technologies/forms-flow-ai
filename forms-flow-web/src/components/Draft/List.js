978e8659d57b2abde50967419d374a4bb5c51e3f
shuhaib s <95394061+shuhaib-aot@users.noreply.github.com>
2023-11-22 16:28:23 +0530

    Fixed draft and submission tab issues (#1766)
    1. filter's ui change with button style on draft and submssions
    2. loader added for pagination also in draft and application
    3. loading fixed for api errors
    4. close filter when action triggered
    5. pagination error fixed for no data

--- forms-flow-web/src/components/Draft/List.js
+++ forms-flow-web/src/components/Draft/List.js
@@ -1,45 +1,30 @@
-/* eslint-disable */
-
-import React, { useEffect, useRef } from "react";
+ 
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import BootstrapTable from "react-bootstrap-table-next";
-import filterFactory from "react-bootstrap-table2-filter";
-import paginationFactory from "react-bootstrap-table2-paginator";
-import ToolkitProvider from "react-bootstrap-table2-toolkit";
+ 
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import Loading from "../../containers/Loading";
-import Nodata from "../Application/nodata";
-import { useTranslation } from "react-i18next";
-import { columns, getoptions } from "./table";
+ import { useTranslation } from "react-i18next"; 
 import { MULTITENANCY_ENABLED } from "../../constants/constants";
-import Alert from "react-bootstrap/Alert";
-import { Translation } from "react-i18next";
-import overlayFactory from "react-bootstrap-table2-overlay";
-import { SpinnerSVG } from "../../containers/SpinnerSVG";
+ 
 import {
   fetchDrafts,
-  FilterDrafts,
 } from "../../apiManager/services/draftService";
 import Confirm from "../../containers/Confirm";
 import Head from "../../containers/Head";
 import { push } from "connected-react-router";
 import {
-  setDraftListLoader,
-  setDraftListActivePage,
-  setCountPerpage,
   setDraftDelete,
   setDraftListLoading,
 } from "../../actions/draftActions";
 import { deleteDraftbyId } from "../../apiManager/services/draftService";
-import isValiResourceId from "../../helper/regExp/validResourceId";
-import { toast } from "react-toastify";
+ import { toast } from "react-toastify";
 import { textTruncate } from "../../helper/helper";
 import DraftTable from "./DraftTable";
 
 export const DraftList = React.memo(() => {
   const { t } = useTranslation();
-  const drafts = useSelector((state) => state.draft.draftList);
-  const countPerPage = useSelector((state) => state.draft.countPerPage);
+   const countPerPage = useSelector((state) => state.draft.countPerPage);
   const draftDelete = useSelector((state) => state.draft?.draftDelete);
 
   const isDraftListLoading = useSelector(
@@ -56,45 +41,27 @@ export const DraftList = React.memo(() => {
   const draftListSearchParams = useSelector(
     (state) => state.draft.searchParams
   );
-  const iserror = useSelector(
-    (state) => state.draft.draftSubmissionError.error
-  );
-  const error = useSelector(
-    (state) => state.draft.draftSubmissionError.message
-  );
-  const [filtermode, setfiltermode] = React.useState(false);
-  const tenantKey = useSelector((state) => state.tenants?.tenantId);
-  const redirectUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : "/";
-  const [lastModified, setLastModified] = React.useState(null);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [invalidFilters, setInvalidFilters] = React.useState({});
-  let filterParams = {
+ 
+   const tenantKey = useSelector((state) => state.tenants?.tenantId);
+   const redirectUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : "/";
+   let filterParams = {
       ...draftListSearchParams,
       page: page,
       limit: countPerPage,
       sortOrder,
       sortBy
-  }
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, [drafts]);
-
-  const useNoRenderRef = (currentValue) => {
-    const ref = useRef(currentValue);
-    ref.current = currentValue;
-    return ref;
   };
 
-  const countPerPageRef = useNoRenderRef(countPerPage);
+ 
 
-  const currentPage = useNoRenderRef(page);
+ 
+ 
 
   useEffect(() => {
-    dispatch(fetchDrafts(filterParams,(err,data) => {
-      if(data){
+    dispatch(fetchDrafts(filterParams,() => {
+  
       dispatch(setDraftListLoading(false));
-      }
+     
     }));
   }, [dispatch, page, countPerPage,sortOrder,sortBy,draftListSearchParams]);
 
@@ -136,51 +103,7 @@ export const DraftList = React.memo(() => {
     return <Loading />;
   }
 
-  const getNoDataIndicationContent = () => {
-    return (
-      <div className="div-no-application">
-        <label className="lbl-no-application">
-          {" "}
-          <Translation>{(t) => t("No drafts found")}</Translation>{" "}
-        </label>
-        <br />
-        {filtermode && (
-          <label className="lbl-no-application-desc">
-            {" "}
-            <Translation>
-              {(t) => t("Please change the selected filters to view drafts")}
-            </Translation>
-          </label>
-        )}
-        <br />
-      </div>
-    );
-  };
-  const validateFilters = (newState) => {
-    if (
-      newState.filters?.id?.filterVal &&
-      !isValiResourceId(newState.filters?.id?.filterVal)
-    ) {
-      return setInvalidFilters({ ...invalidFilters, DRAFT_ID: true });
-    } else {
-      return setInvalidFilters({ ...invalidFilters, DRAFT_ID: false });
-    }
-  };
-  // const handlePageChange = (type, newState) => {
-  //   validateFilters(newState);
-  //   if (type === "filter") {
-  //     setfiltermode(true);
-  //   } else if (type === "pagination") {
-  //     if (countPerPage > 5) {
-  //       dispatch(setDraftListLoader(true));
-  //     } else {
-  //       setIsLoading(true);
-  //     }
-  //   }
-  //   dispatch(setCountPerpage(newState.sizePerPage));
-  //   dispatch(FilterDrafts(newState));
-  //   dispatch(setDraftListActivePage(newState.page));
-  // };
+ 
   const headerList = () => {
     return [
       {
@@ -200,14 +123,7 @@ export const DraftList = React.memo(() => {
   let headOptions = headerList();
 
 
-  const getNoData = () => {
-    if (iserror) {
-      return <Alert variant={"danger"}>{error}</Alert>;
-    } else {
-      return <Nodata text={t("No Drafts Found")} />;
-    }
-  };
-
+ 
   return (
     <>
      <Confirm

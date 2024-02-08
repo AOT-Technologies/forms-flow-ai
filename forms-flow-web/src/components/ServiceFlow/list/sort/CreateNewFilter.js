63fb39b60a273e7b22b26aca2c9d3b6ff3ceec94
fahad-aot <fahad.k@aot-technologies.com>
2024-02-08 02:06:31 -0800

    Value in the Definition key or assignee will be visible on editing the filter

--- forms-flow-web/src/components/ServiceFlow/list/sort/CreateNewFilter.js
+++ forms-flow-web/src/components/ServiceFlow/list/sort/CreateNewFilter.js
@@ -534,7 +534,7 @@ export default function CreateNewFilterDrawer({
         <h5 className="mt-2" style={{ fontSize: "18px" }}>
           <Translation>{(t) => t("Definition Key")}</Translation>
         </h5>
-        <span
+        {!definitionKeyId && (<span
           style={{
             textDecoration: "underline",
             fontSize: "14px",
@@ -544,18 +544,18 @@ export default function CreateNewFilterDrawer({
         >
           <i className="fa fa-plus-circle" style={{ marginRight: "6px" }} />
           <Translation>{(t) => t("Add Value")}</Translation>
-        </span>
-        {inputVisibility[1] && (
+        </span>) }        
+        {(inputVisibility[1] || definitionKeyId ) && (
           <input
             type="text"
             className="criteria-add-value-inputbox"
             value={definitionKeyId}
             name="definitionKeyId"
             onChange={(e) => setDefinitionKeyId(e.target.value)}
-            title={t("Candidate Group")}
+            title={t("Definition Key")}
           />
         )}
-        <h5>
+        <h5 className="pt-2">
           <Translation>{(t) => t("Candidate Group")}</Translation>
         </h5>
 
@@ -612,10 +612,10 @@ export default function CreateNewFilterDrawer({
             </Badge>
           </div>
         )}
-        <h5>
+        <h5 className="pt-2">
           <Translation>{(t) => t("Assignee")}</Translation>
         </h5>
-        <span
+        { !assignee && <span
           style={{
             textDecoration: "underline",
             fontSize: "14px",
@@ -625,13 +625,14 @@ export default function CreateNewFilterDrawer({
         >
           <i className="fa fa-plus-circle" style={{ marginRight: "6px" }} />
           <Translation>{(t) => t("Add Value")}</Translation>
-        </span>
-        {inputVisibility[3] && (
+        </span>}
+        {(inputVisibility[3] || assignee) && (
           <input
             type="text"
             className="criteria-add-value-inputbox"
             value={assignee}
             onChange={(e) => setAssignee(e.target.value)}
+            title={t("Assignee")}
           />
         )}
 

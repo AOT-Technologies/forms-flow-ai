698b646d636fccc50780cdd6fe9bfa65fb976f45
Garrett Bates <gbates101@gmail.com>
2024-02-09 15:40:18 -0500

    move slash to end of default

--- forms-flow-api/src/formsflow_api/services/external/keycloak.py
+++ forms-flow-api/src/formsflow_api/services/external/keycloak.py
@@ -42,8 +42,8 @@ class KeycloakAdminAPIService:
             }
         )
         self.base_url = (
-            f"{current_app.config.get('KEYCLOAK_URL')}"
-            f"{current_app.config.get('KEYCLOAK_URL_HTTP_RELATIVE_PATH', '/auth')}/admin/realms/"
+            f"{current_app.config.get('KEYCLOAK_URL')}/"
+            f"{current_app.config.get('KEYCLOAK_URL_HTTP_RELATIVE_PATH', 'auth/')}/admin/realms/"
             f"{current_app.config.get('KEYCLOAK_URL_REALM')}"
         )
 

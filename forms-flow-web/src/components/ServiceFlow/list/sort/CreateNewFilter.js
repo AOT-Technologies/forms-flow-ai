88473d452c0ea6f753542fb010d5494df35ac38b
josephalexantony-aot <alex.antony@aot-technologies.com>
2024-03-18 01:58:19 -0700

    imported useMemo

--- forms-flow-web/src/components/ServiceFlow/list/sort/CreateNewFilter.js
+++ forms-flow-web/src/components/ServiceFlow/list/sort/CreateNewFilter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";

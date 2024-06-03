cd93dc9f423e9bf7ba0a64efed32b799260d9f79
abilpraju-aot <abil.raju@aot-technologies.com>
2024-06-03 19:39:44 +0530

    k

--- forms-flow-api/setup.py
+++ forms-flow-api/setup.py
@@ -1,9 +1,9 @@
-"""Installer and setup for this module."""
+from setuptools import find_packages, setup
 
 from glob import glob
 from os.path import basename, splitext
 
-from setuptools import find_packages, setup
+										   
 
 
 def read_requirements(filename):
@@ -36,6 +36,9 @@ setup(
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
+    package_data={
+        'formsflow_api_utils': ['utils/permissions.json'],
+    },
     long_description=read("README.md"),
     zip_safe=False,
     install_requires=REQUIREMENTS,

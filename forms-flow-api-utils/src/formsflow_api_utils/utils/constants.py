969ab5b1c6c5645d4d68d2530063085d942d0302
abilpraju-aot <abil.raju@aot-technologies.com>
2024-06-03 12:22:46 +0530

    updated code based on reading from json

--- forms-flow-api-utils/src/formsflow_api_utils/utils/constants.py
+++ forms-flow-api-utils/src/formsflow_api_utils/utils/constants.py
@@ -1,11 +1,23 @@
 """All constants for project."""
 import os
-
+import json
 from dotenv import find_dotenv, load_dotenv
 
 # this will load all the envars from a .env file located in the project root (api)
 load_dotenv(find_dotenv())
 
+# Load JSON data from a file
+def load_json_data(filepath):
+    with open(filepath, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+# Determine the path to the permissions.json file
+current_directory = os.path.dirname(os.path.realpath(__file__))
+file_path = os.path.join(current_directory, 'permissions.json')
+
+# Load the permissions data
+PERMISSIONS = load_json_data(file_path)
 FORMSFLOW_API_CORS_ORIGINS = os.getenv("FORMSFLOW_API_CORS_ORIGINS", "*")
 ALLOW_ALL_ORIGINS = "*"
 CORS_ORIGINS = []
@@ -48,73 +60,3 @@ FILTER_MAPS = {
 DEFAULT_PROCESS_KEY = "Defaultflow"
 DEFAULT_PROCESS_NAME = "Default Flow"
 HTTP_TIMEOUT = 30
-PERMISSIONS = [{
-      "name": "create_designs",
-      "description": "Create Form, workflow designs",
-      "depends_on": ["view_designs"]
-    },
-    {
-      "name": "view_designs",
-      "description": "Access to design",
-      "depends_on": []
-    },
-    {
-      "name": "create_submissions",
-      "description": "Create submissions",
-      "depends_on": []
-    },
-    {
-      "name": "view_submissions",
-      "description": "Access to submissions",
-      "depends_on": []
-    },
-    {
-      "name": "view_dashboards",
-      "description": "Access to dashboards",
-      "depends_on": []
-    },
-    {
-      "name": "view_tasks",
-      "description": "Access to tasks",
-      "depends_on": []
-    },
-    {
-      "name": "manage_tasks",
-      "description": "Can claim and work on tasks",
-      "depends_on": ["view_tasks"]
-    },{
-      "name": "manage_all_filters",
-      "description": "Manage all filters",
-      "depends_on": ["view_filters","create_filters"]
-    },
-    {
-      "name": "create_filters",
-      "description": "Access to create filters",
-      "depends_on": ["view_filters"]
-    },
-    {
-      "name": "view_filters",
-      "description": "Access to view filters",
-      "depends_on": []
-    },
-    {
-      "name": "manage_integrations",
-      "description": "Access to Integrations",
-      "depends_on": []
-    },
-    {
-      "name": "manage_dashboard_authorizations",
-      "description": "Manage Dashboard Authorization",
-      "depends_on": ["view_dashboards"]
-    },
-    {
-      "name": "manage_users",
-      "description": "Manage Users",
-      "depends_on": []
-    },
-    {
-      "name": "manage_roles",
-      "description": "Manage Roles",
-      "depends_on": ["manage_users"]
-    }
-]

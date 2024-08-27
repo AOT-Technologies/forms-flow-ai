6a9f421075986350e5b1a4577916afa7b4590ce0
shuhaib s <95394061+shuhaib-aot@users.noreply.github.com>
2024-08-27 15:35:28 +0530

    removed unwanted print

--- forms-flow-api/src/formsflow_api/resources/formio.py
+++ forms-flow-api/src/formsflow_api/resources/formio.py
@@ -146,8 +146,6 @@ class FormioResource(Resource):
             formio_user_resource_id = Cache.get("user_resource_id")
 
         roles = get_role_ids_from_user_groups(role_ids, user_role)
-        print(roles,"roles",user_role,"user_roles",role_ids)
-        print(user)
         if roles is not None:
             roles.append(
                 {

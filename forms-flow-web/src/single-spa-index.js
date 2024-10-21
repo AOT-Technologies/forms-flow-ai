71c281535456620af15a749cda679c4ba925616b
abilpraju-aot <abil.raju@aot-technologies.com>
2024-10-21 00:19:28 -0700

    updated the single spa file

--- forms-flow-web/src/single-spa-index.js
+++ forms-flow-web/src/single-spa-index.js
@@ -7,6 +7,7 @@ import { featureFlags } from "./featureToogle";
 import { FlagsProvider } from 'flagged';
 import { Formio, Components } from "@aot-technologies/formio-react";
 import { AppConfig } from "./config";
+import "./resourceBundles/i18n.js";
 
 if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === "object") {
     for (let [key, value] of Object.entries(

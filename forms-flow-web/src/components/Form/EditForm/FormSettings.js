092b9bfca46a54d3dcbccd27e3fc3f11fcea3758
Bonymol-aot <bonymol@aot-technologies.com>
2024-10-24 22:45:49 -0700

    FWF-3757: [Feature] Added validations for form name and path, removed placeholder

--- forms-flow-web/src/components/Form/EditForm/FormSettings.js
+++ forms-flow-web/src/components/Form/EditForm/FormSettings.js
@@ -21,6 +21,7 @@ import { useTranslation } from "react-i18next";
 import { copyText } from "../../../apiManager/services/formatterService";
 import _camelCase from "lodash/camelCase";
 import _cloneDeep from "lodash/cloneDeep";
+import { validateFormName, validatePathName } from "../../../apiManager/services/FormServices";
 
 //CONST VARIABLES
 const DESIGN = "DESIGN";
@@ -55,7 +56,10 @@ const FormSettings = forwardRef((props, ref) => {
     display: display,
   });
   const [isAnonymous, setIsAnonymous] = useState(processListData.anonymous);
-
+  const [errors, setErrors] = useState({
+    name: "",
+    path: "",
+  });
   const [formAccessCopy, setFormAccessCopy] = useState(_cloneDeep(formAccess));
   const [submissionAccessCopy, setSubmissionAccessCopy] = useState(
     _cloneDeep(submissionAccess)
@@ -81,21 +85,48 @@ const FormSettings = forwardRef((props, ref) => {
     },
   });
 
+    /* ------------------------- validating form name and path ------------------------ */
+
+  const validateField = async (field, value) => {
+    let errorMessage = "";
+    if (!value.trim()) {
+      errorMessage = `${field} is required`;
+    } else {
+      try {
+        const response = field === 'name' ? await validateFormName(value) : await validatePathName(value);
+        const data = response?.data;
+        if (data && data.code === "FORM_EXISTS") {
+          errorMessage = data.message;
+        }
+      } catch (error) {
+        errorMessage = error.response?.data?.message || 
+        `An error occurred while validating the ${field}.`;
+        console.error(`Error validating ${field}:`, errorMessage);
+      }
+    }
+    setErrors((prev) => ({ ...prev, [field]: errorMessage }));
+  };
+
+
+  
   /* ---------------------- handling form details change ---------------------- */
+
   const handleFormDetailsChange = (e) => {
     const { name, value, type } = e.target;
     // if form name or path changed need to call validation api based on that
     let updatedValue =
       name === "path" ? _camelCase(value).toLowerCase() : value;
+    
     if (type === "checkbox") {
-      updatedValue = e.target.checked ? "wizard" : "form";
+      setFormDetails((prev) => ({ ...prev, [name]: e.target.checked ? "wizard" : "form" }));
+    } else {
+      setFormDetails((prev) => ({ ...prev, [name]: updatedValue }));
+      if (name === "title" || name === "path") {
+        validateField(name === "title" ? 'name' : 'path', updatedValue);
+      }
     }
-    setFormDetails((prev) => ({ ...prev, [name]: updatedValue }));
-
-    // if(name == "path" || name =="title"){
-    //   validationapi call
-    // }
   };
+  
 
   /* ---------------------------- Fetch user roles ---------------------------- */
   useEffect(() => {
@@ -178,7 +209,9 @@ const FormSettings = forwardRef((props, ref) => {
           dataTestid="form-name"
           name="title"
           ariaLabel={t("Form Name")}
-        />
+          onBlur={() => validateField('name', formDetails.title)}
+          />
+          {errors.name && <div className="validation-text">{errors.name}</div>}
         <FormTextArea
           label={t("Description")}
           name="description"
@@ -390,11 +423,13 @@ const FormSettings = forwardRef((props, ref) => {
               className="url-edit"
               name="path"
               onChange={handleFormDetailsChange}
-            />
+              onBlur={() => validateField('path', formDetails.path)}            />
             <InputGroup.Text className="url-copy" onClick={copyPublicUrl}>
               {copied ? <i className="fa fa-check" /> : <CopyIcon />}
             </InputGroup.Text>
           </InputGroup>
+          {errors.path && <div className="validation-text">{errors.path}</div>}
+
         </Form.Group>
       </div>
     </>

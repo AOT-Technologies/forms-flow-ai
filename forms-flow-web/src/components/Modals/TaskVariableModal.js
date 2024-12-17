c72128cd03dfd315c1380666b10a8db1beacfd92
fahad-aot <fahad.k@aot-technologies.com>
2024-12-16 20:29:48 -0800

    bugfix/fwf-4089-Hide-default-task-var

--- forms-flow-web/src/components/Modals/TaskVariableModal.js
+++ forms-flow-web/src/components/Modals/TaskVariableModal.js
@@ -19,15 +19,19 @@ import {
 //TBD in case of Bundle form display
 const PillList = React.memo(({ alternativeLabels, onRemove }) => {
   const { t } = useTranslation();
+  // Filter out applicationId and applicationStatus
+  const filteredVariablePills = Object.values(alternativeLabels).filter(
+    ({ key }) => key !== "applicationId" && key !== "applicationStatus"
+  );
   return (
     <div className="pill-container">
-      {Object.values(alternativeLabels).length ? (
-        Object.values(alternativeLabels).map(
+      {filteredVariablePills.length ? (
+        filteredVariablePills.map(
           ({key, altVariable, labelOfComponent }) => (
             <CustomPill
               key={key}
               label={altVariable || labelOfComponent}
-              icon={(key !== "applicationId" && key !== "applicationStatus") && <CloseIcon color="#253DF4" data-testid="pill-remove-icon" />}
+              icon={<CloseIcon color="#253DF4" data-testid="pill-remove-icon" />}
               bg="#E7E9FE"
               onClick={() => onRemove(key)}
               secondaryLabel={key}

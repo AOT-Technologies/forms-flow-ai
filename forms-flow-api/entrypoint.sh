5c273846603f2812ffd8bfc94da40cc2019e1ed7
Kiran K A <kiran.ka@aot-technologies.com>
2025-01-22 18:11:17 +0530

    Externalize api starter config entries

--- forms-flow-api/entrypoint.sh
+++ forms-flow-api/entrypoint.sh
@@ -3,4 +3,4 @@ echo 'starting database migration.'
 export FLASK_APP=manage.py
 flask db upgrade
 echo 'database migration completed.'
-gunicorn -b :5000 'formsflow_api:create_app()' --timeout 120 --worker-class=gthread --workers=5 --threads=10
+gunicorn -b :5000 'formsflow_api:create_app()' --timeout 120 --worker-class=gthread --workers=$GUNICORN_WORKERS --threads=$GUNICORN_THREADS

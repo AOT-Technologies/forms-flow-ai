d7b8925e6398084ec230f93c09b671ef3c34ed81
Kiran K A <kiran.ka@aot-technologies.com>
2025-01-23 14:52:19 +0530

    Externalized Gunicorn timeout configuration to allow customization via the environment file

--- forms-flow-api/entrypoint.sh
+++ forms-flow-api/entrypoint.sh
@@ -3,4 +3,4 @@ echo 'starting database migration.'
 export FLASK_APP=manage.py
 flask db upgrade
 echo 'database migration completed.'
-gunicorn -b :5000 'formsflow_api:create_app()' --timeout 120 --worker-class=gthread --workers=$GUNICORN_WORKERS --threads=$GUNICORN_THREADS
+gunicorn -b :5000 'formsflow_api:create_app()' --timeout $GUNICORN_TIMEOUT --worker-class=gthread --workers=$GUNICORN_WORKERS --threads=$GUNICORN_THREADS

105702a3064e5c77527d2aee102175c841b1433a
auslin-aot <99173163+auslin-aot@users.noreply.github.com>
2025-03-12 10:59:06 +0530

    FWF-1899: Pylint fix, Updated requirements file

--- forms-flow-data-layer/src/db/__init__.py
+++ forms-flow-data-layer/src/db/__init__.py
@@ -2,5 +2,6 @@ from src.db.bpmn_db import bpmn_db
 from src.db.formio_db import FormioDbConnection
 from src.db.redis_client import redis_cache
 from src.db.webapi_db import webapi_db
+from src.db.sql_base import ConnectSQLDatabase
 
-__all__ = ["FormioDbConnection", "redis_cache", "bpmn_db", "webapi_db"]
+__all__ = ["FormioDbConnection", "redis_cache", "bpmn_db", "webapi_db", "ConnectSQLDatabase"]

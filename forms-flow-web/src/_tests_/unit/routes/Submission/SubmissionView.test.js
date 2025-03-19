88a5f2ae66beac90bdd5e7cf32bf0a55d8e7f4b0
abilpraju-aot <abil.raju@aot-technologies.com>
2025-03-19 22:09:01 +0530

    lint fix

--- forms-flow-web/src/_tests_/unit/routes/Submission/SubmissionView.test.js
+++ forms-flow-web/src/_tests_/unit/routes/Submission/SubmissionView.test.js
@@ -144,69 +144,12 @@ function renderWithProviders(ui, { route = "/application/804" } = {}) {
 }
 
 // Initialize store before each test
-let store;
+
 beforeEach(() => {
-  store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
-    preloadedState: {
-      ...mockstate,
-      applications: {
-        applicationsList: [],
-        applicationDetail:
-          {
-            created: "2025-02-11T05:15:55.785503Z",
-            modified: "2025-02-11T05:22:34.207536Z",
-            id: 804,
-            applicationName: "BusinessNew",
-            applicationStatus: "Reviewed",
-            formProcessMapperId: "531",
-            processInstanceId: "52e9e47b-e837-11ef-9ea0-620c3eb0f440",
-            processKey: "BusinessNew",
-            processName: "BusinessNew",
-            processTenant: null,
-            createdBy: "john.honai",
-            modifiedBy: "service-account-forms-flow-bpm",
-            formId: "6721debe9c3135103599ba2b",
-            submissionId: "67aade995c702e11810155b2",
-            isResubmit: false,
-            eventName: null,
-          },
-        applicationProcess: {
-          processName: "BusinessNew",
-          formProcessMapperId: "531",
-          processKey: "BusinessNew",
-        },
-        formApplicationsList: [],
-        isApplicationListLoading: false,
-        isApplicationDetailLoading: false,
-        isApplicationUpdating: false,
-        applicationCount: 0,
-        applicationDetailStatusCode: 200,
-        activePage: 1,
-        countPerPage: 5,
-        applicationStatus: [],
-        iserror: false,
-        error: "",
-        isPublicStatusLoading: false,
-        sortOrder: "desc",
-        sortBy: "id",
-        searchParams: {},
-        isApplicationLoading: false,
-      },
-      submission: { submission: { _id: "67aade995c702e11810155b2" } },
-      form: { form: { _id: "6721debe9c3135103599ba2b", title: "Business Form" } },
-      taskAppHistory: { appHistory: [], isHistoryListLoading: false },
-      tenants: { tenantId: "tenant123" },
-      user: {
-        roles: ["formsflow-reviewer"],
-        authenticated: true,
-        viewSubmissions: true,
-        createSubmissions: true,
-      },
-    },
-  });
+  useDispatch.mockReturnValue(mockDispatch);
+  jest.clearAllMocks();
 });
+
 
 const mockDispatch = jest.fn();
 

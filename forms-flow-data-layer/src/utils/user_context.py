e2c65feaee25bab8b300213554c131413b5586ef
shuhaib-aot <shuhaib.s@aot-technologies.com>
2025-03-19 11:27:27 +0530

    Fixed sonar issue

--- forms-flow-data-layer/src/utils/user_context.py
+++ forms-flow-data-layer/src/utils/user_context.py
@@ -7,7 +7,7 @@ class UserContext:  # pylint: disable=too-many-instance-attributes
     """Object to hold request scoped user context."""
 
     def __init__(self, token, token_info):
-        """Return a User Context object."""
+        """Initialize User Context object."""
         self._tenant_key = token_info.get("tenantKey", None)
         self._user_name = token_info.get("preferred_username", None)
         self._bearer_token: str = token
@@ -18,41 +18,39 @@ class UserContext:  # pylint: disable=too-many-instance-attributes
         )
         self._groups: list = token_info.get("groups", None)
 
+    def _get_attr(self, attr_name: str):
+        """Generic attribute getter."""
+        return getattr(self, attr_name)
+
     @property
     def tenant_key(self) -> str:
-        """Return the tenant key."""
-        return self._tenant_key
+        return self._get_attr("_tenant_key")
 
     @property
     def user_name(self) -> str:
-        """Return the user name."""
-        return self._user_name
+        return self._get_attr("_user_name")
 
     @property
     def bearer_token(self) -> str:
-        """Return the bearer_token."""
-        return self._bearer_token
+        return self._get_attr("_bearer_token")
 
     @property
     def email(self) -> str:
-        """Return the email."""
-        return self._email
+        return self._get_attr("_email")
 
     @property
     def roles(self) -> List[str]:
-        """Return the roles."""
-        return self._roles
+        return self._get_attr("_roles")
 
     @property
     def groups(self) -> List[str]:
-        """Return the groups."""
-        return self._groups
+        return self._get_attr("_groups")
 
     @property
     def group_or_roles(self) -> List[str]:
-        """Return groups is env is using groups, else roles."""
+        """Return groups if env is using groups, else roles."""
         if ENVS.get("KEYCLOAK_ENABLE_CLIENT_AUTH") and not ENVS.get(
             "MULTI_TENANCY_ENABLED"
         ):
-            return self._roles
-        return self._groups
+            return self.roles
+        return self.groups

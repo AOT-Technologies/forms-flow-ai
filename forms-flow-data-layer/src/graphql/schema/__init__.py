45db01f4375023e7d9ac9f33c72890f49ebbe426
auslin-aot <99173163+auslin-aot@users.noreply.github.com>
2025-04-10 08:44:36 +0530

    Fwf 4446 add query support data layer (#2691)
    * FWF-4446: [Feature]  Added QuerySubmissionsResolver to handle submission queries.
    
    * FWF-4446: [Feature] Enhanced SubmissionService with form authorization query method
    
    * FWF-4446: [Feature] Updated QuerySubmissionsResolver and SubmissionService to support optional filters for submitted_by and location, enhancing submission querying capabilities with detailed documentation.
    
    * FWF-4446: [Feature] Enhanced submission querying capabilities
    
    * FWF-4446: [Feature] Added optional parent_form_id filter to submission queries.
    
    * FWF-4446: [Feature] Implemented pagination support for submission queries, enhancing QuerySubmissionsResolver  to return paginated results with total count and page information.
    
    * FWF-4446: [Refactor] Removed caching QuerySubmissionsResolver to streamline submission query handling.
    
    * FWF-4446: [Feature] Enhanced submission querying by introducing search and project fields parameters, allowing for more flexible data retrieval from both SQL and MongoDB.
    
    * FWF-4446: [Refactor] Sonar Fix
    
    * FWF-4446: [Feature] Added testcase

--- forms-flow-data-layer/src/graphql/schema/__init__.py
+++ forms-flow-data-layer/src/graphql/schema/__init__.py
@@ -1,4 +1,13 @@
 from src.graphql.schema.formio_form_schema import FormSchema
-from src.graphql.schema.submission_schema import SubmissionSchema
+from src.graphql.schema.submission_schema import (
+    PaginatedSubmissionResponse,
+    QuerySubmissionsSchema,
+    SubmissionSchema,
+)
 
-__all__ = ["FormSchema", "SubmissionSchema"]
+__all__ = [
+    "FormSchema",
+    "SubmissionSchema",
+    "QuerySubmissionsSchema",
+    "PaginatedSubmissionResponse",
+]
--- forms-flow-data-layer/src/models/formio/submission.py
+++ forms-flow-data-layer/src/models/formio/submission.py
@@ -1,8 +1,9 @@
-from beanie import Document
+from beanie import Document, PydanticObjectId
 
 
 class SubmissionsModel(Document):
     data: dict
+    _id: PydanticObjectId
 
     class Settings:
         name = "submissions"

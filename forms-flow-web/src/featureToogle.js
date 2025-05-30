18052f83547e50b7d113dce4de93cdf0949e472b
abilpraju-aot <abil.raju@aot-technologies.com>
2025-05-30 02:57:26 -0700

    feature/FWF-4672 [modified] updated styling on documents api

--- forms-flow-web/src/featureToogle.js
+++ forms-flow-web/src/featureToogle.js
@@ -11,6 +11,6 @@ function getEnv(env_string) {
 // Either take values from env or can directly give true or false
 export const featureFlags = {
   exportPdf: getEnv('REACT_APP_EXPORT_PDF_ENABLED'),
-  enableCompactViewForm: getEnv('REACT_APP_ENABLE_COMPACT_VIEW_FORM'),
+  enableCompactViewForm: getEnv('REACT_APP_ENABLE_COMPACT_FORM_VIEW'),
   enableApplicationAccessPermissionCheck: getEnv('REACT_APP_ENABLE_APPLICATION_ACCESS_PERMISSION_CHECK')
 };

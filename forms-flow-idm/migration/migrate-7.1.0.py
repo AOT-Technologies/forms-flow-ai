ef106d84e17d17962e382561361d459cbc2eb419
auslin-aot <99173163+auslin-aot@users.noreply.github.com>
2025-07-21 15:23:29 +0530

    Remove deprecated delete_client_role function from migration script

--- forms-flow-idm/migration/migrate-7.1.0.py
+++ forms-flow-idm/migration/migrate-7.1.0.py
@@ -1,5 +1,5 @@
 
-from utils import get_access_token, get_client_id, add_client_roles, delete_client_role, update_client_role, role_changes_710
+from utils import get_access_token, get_client_id, add_client_roles, update_client_role, role_changes_710
 
 # Roles to be added, removed, or updated for the forms-flow-web client
 roles_to_add, roles_to_remove, roles_to_update = role_changes_710()
@@ -11,7 +11,6 @@ def _migrate_default():
         # Get client and group IDs
         client_id = get_client_id(token, "forms-flow-web")
         add_client_roles(token, client_id, roles_to_add)
-        delete_client_role(token,client_id, roles_to_remove)
         update_client_role(token, client_id, roles_to_update)
         
 

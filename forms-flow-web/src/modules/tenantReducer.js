0103769093e80e125b0f331d2309ab4cea957f62
abilpraju-aot <abil.raju@aot-technologies.com>
2025-07-25 01:27:56 -0700

    removed the session storage

--- forms-flow-web/src/modules/tenantReducer.js
+++ forms-flow-web/src/modules/tenantReducer.js
@@ -35,7 +35,6 @@ const tenants = (state = initialState, action) => {
       };
     case ACTION_CONSTANTS.SET_TENANT_DATA:
       localStorage.setItem("tenantData", JSON.stringify(action.payload));
-      sessionStorage.setItem("tenantData", JSON.stringify(action.payload));
       return {
         ...state,
         tenantData: action.payload,
